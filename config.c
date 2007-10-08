46cde2d95c265e1023ae31d55e9bf32cc97305a0
Julien Danjou <julien@danjou.info>
2007-10-08 10:26:47 +0200

    bugfix: resize_hint is a bool, not a float

--- config.c
+++ config.c
@@ -400,7 +400,7 @@ parse_config(Display * disp, int scr, DC * drawcontext, const char *confpatharg,
     awesomeconf->mwfact = f ? f : 0.6;
 
     /* resize_hints */
-    awesomeconf->resize_hints = config_lookup_float(&awesomelibconf, "awesome.resize_hints");
+    awesomeconf->resize_hints = config_lookup_bool(&awesomelibconf, "awesome.resize_hints");
 
     /* colors */
     tmp = config_lookup_string(&awesomelibconf, "awesome.normal_border_color");
