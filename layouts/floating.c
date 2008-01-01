e5af2dcf24884fd5dc876886a531b0655dba6c3e
Julien Danjou <julien@danjou.info>
2008-01-01 16:56:11 +0100

    fix maximized windows in floating layout

--- layouts/floating.c
+++ layouts/floating.c
@@ -30,7 +30,7 @@ layout_floating(int screen)
     Client *c;
 
     for(c = globalconf.clients; c; c = c->next)
-        if(client_isvisible(c, screen))
+        if(client_isvisible(c, screen) && !c->ismax)
             client_resize(c, c->rx, c->ry, c->rw, c->rh, True, False);
 }
 // vim: filetype=c:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:encoding=utf-8:textwidth=80
