9865d44a888434e433a5859b92f057e85fa9830d
Julien Danjou <julien@danjou.info>
2008-01-02 11:32:12 +0100

    Revert "remove borders modification on maximize()"
    This reverts commit d8ef2748a661cd27e7729fae92c8acdd49162a2b.

--- client.c
+++ client.c
@@ -855,6 +855,8 @@ client_maximize(Client *c, int x, int y, int w, int h)
 {
     if((c->ismax = !c->ismax))
     {
+        c->oldborder = c->border;
+        c->border = 0;
         c->wasfloating = c->isfloating;
         c->isfloating = True;
         client_resize(c, x, y, w, h, False, True);
@@ -863,6 +865,8 @@ client_maximize(Client *c, int x, int y, int w, int h)
         client_resize(c, c->rx, c->ry, c->rw, c->rh, True, False);
     else
         c->isfloating = False;
+
+    c->border = c->oldborder;
 
     arrange(c->screen);
 }
