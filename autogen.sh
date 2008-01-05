88a0b1cd12a72392a6c9663f4761e083597fea44
marco candrian <mac@calmar.ws>
2008-01-05 11:35:00 +0100

    autogen.sh added

---
+++ autogen.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+echo "Generating configure files... may take a while."
+
+autoreconf --install --force && \
+  echo "Preparing was successful if there was no error messages above." && \
+  echo "Now type:" && \
+  echo "  ./configure && make"  && \
+  echo "Run './configure --help' for more information"
