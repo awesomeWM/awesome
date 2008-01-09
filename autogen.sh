b55774c0e0c1fc528f997cbdebc7f1cd65b8ddf1
Hans Ulrich Niedermann <hun@n-dimensional.de>
2008-01-09 16:25:48 +0100

    autogen.sh: Change to source directory first
    Change to the source tree's top directory before doing anything else.

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# Change to source tree
+srcdir=`dirname "$0"`
+[ -z "$srcdir" ] || cd "$srcdir"
+
 echo "Generating configure files... may take a while."
 
 autoreconf --install --force && \
