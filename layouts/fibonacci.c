d99374bff2aa8ea50751580394f26de6b46b54fc
Julien Danjou <julien@danjou.info>
2008-01-12 14:24:13 +0100

    readd border size on geom

--- layouts/fibonacci.c
+++ layouts/fibonacci.c
@@ -83,6 +83,8 @@ layout_fibonacci(int screen, int shape)
             geometry.width -= 2 * c->border;
             geometry.height -= 2 * c->border;
             client_resize(c, geometry, globalconf.screens[screen].resize_hints);
+            geometry.width += 2 * c->border;
+            geometry.height += 2 * c->border;
         }
 }
 
