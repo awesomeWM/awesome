7a4237f34f2fc3bcac0c15ccd66f045950888a45
Julien Danjou <julien@danjou.info>
2008-04-28 17:50:46 +0200

    [markup] Print the unparseable text on error
    Signed-off-by: Julien Danjou <julien@danjou.info>

--- common/markup.c
+++ common/markup.c
@@ -233,7 +233,7 @@ markup_parse(markup_parser_data_t *data, const char *str, ssize_t slen)
        || !g_markup_parse_context_parse(mkp_ctx, "</markup>", -1, &error)
        || !g_markup_parse_context_end_parse(mkp_ctx, &error))
     {
-        warn("unable to parse text: %s\n", error->message);
+        warn("unable to parse text \"%s\": %s\n", str, error->message);
         g_error_free(error);
         return false;
     }
