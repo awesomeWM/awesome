1a8467ff280d969d7fff8f7c8a36ffb08c9873d0
Julien Danjou <julien@danjou.info>
2008-07-29 16:13:27 +0200

    build: dist create bz2 too
    Signed-off-by: Julien Danjou <julien@danjou.info>

--- build-utils/dist.sh
+++ build-utils/dist.sh
@@ -4,8 +4,9 @@
 
 set -e
 
-VERSION=$1
+VERSION=`echo $1 | sed 's/^v//'`
 git-archive --prefix=dist/awesome-$VERSION/ $VERSION | tar -xf -
 cd dist
 echo -n $VERSION > awesome-$VERSION/.version_stamp
 tar czf awesome-$VERSION.tar.gz awesome-$VERSION
+tar cjf awesome-$VERSION.tar.bz2 awesome-$VERSION
