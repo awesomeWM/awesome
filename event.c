cb1de2633daca2a447042e392b11fac5e668ecec
Kanru Chen <koster@debian.org.tw>
2008-08-03 21:04:52 +0800

    event: Ungrab root when mouse enters client window.
    Signed-off-by: Julien Danjou <julien@danjou.info>

--- event.c
+++ event.c
@@ -337,7 +337,7 @@ event_handle_enternotify(void *data __attribute__ ((unused)),
     if((c = client_getbytitlebarwin(ev->event))
        || (c = client_getbywin(ev->event)))
     {
-        window_grabbuttons(c->win, c->phys_screen, c->buttons);
+        window_grabbuttons(c->win, ev->root, c->buttons);
         /* The idea behind saving pointer_x and pointer_y is Bob Marley powered.
          * this will allow us top drop some EnterNotify events and thus not giving
          * focus to windows appering under the cursor without a cursor move */
