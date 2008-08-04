4abea3accab7b282b4df8cbfb059b54b21cb3723
Julien Danjou <julien@danjou.info>
2008-08-04 14:31:53 +0200

    libs: rewrite as module
    Signed-off-by: Julien Danjou <julien@danjou.info>

--- lib/tabulous.lua
+++ lib/tabulous.lua
@@ -1,38 +1,26 @@
-----------------------------------------------------
---      tabulous: Fabulous tabs for awesome       --
---                                                --
--- © 2008 Julien Danjou <julien@danjou.info>      --
--- © 2008 Lucas de Vries <lucasdevries@gmail.com> --
-----------------------------------------------------
-
-require('awful')
-
---------------------
--- Module loading --
---------------------
-local P = {}
-if _REQUIREDNAME == nil then
-    tabulous = P
-else
-    _G[_REQUIREDNAME] = P
-end
+---------------------------------------------------------------------------
+-- tabulous: fabulous tabs for awesome
+--
+-- @author Lucas de Vries <lucasdevries@gmail.com>
+-- @author Julien Danjou <julien@danjou.info>
+-- @copyright 2008 Julien Danjou, Lucas de Vries
+---------------------------------------------------------------------------
 
 -- Grab environment we need
-local client = client
+local capi = { client = client }
 local table = table
 local pairs = pairs
-local awful = awful
+local awful = require('awful')
 
--- Reset env
-setfenv(1, P)
+module("tabulous")
 
-tabbed = {}
+local tabbed = {}
 
 -- Hook creation
-awful.hooks.userhook_create('tabbed')
-awful.hooks.userhook_create('untabbed')
-awful.hooks.userhook_create('tabdisplay')
-awful.hooks.userhook_create('tabhide')
+awful.hooks.user.create('tabbed')
+awful.hooks.user.create('untabbed')
+awful.hooks.user.create('tabdisplay')
+awful.hooks.user.create('tabhide')
 
 ---------------
 -- Functions --
@@ -42,7 +30,7 @@ awful.hooks.userhook_create('tabhide')
 -- @param table The table to look into
 -- @param value The value to find.
 -- @return The key or nil if not found.
-function P.findkey(table, value)
+function findkey(table, value)
     for k, v in pairs(table) do
         if v == value then
             return k
@@ -50,12 +38,11 @@ function P.findkey(table, value)
     end
 end
 
---- Swap and select which client in tab is displayed.
+--- Swaand select which client in tab is displayed.
 -- @param tabindex The tab index.
 -- @param cl The client to show.
-function P.display(tabindex, cl)
+function display(tabindex, cl)
     local p = tabbed[tabindex][1]
-    
     if cl and p ~= cl then
         cl.hide = false
         cl:swap(p)
@@ -64,8 +51,8 @@ function P.display(tabindex, cl)
 
         tabbed[tabindex][1] = cl
 
-        awful.hooks.userhook_call('tabhide', {p})
-        awful.hooks.userhook_call('tabdisplay', {cl})
+        awful.hooks.user.call('tabhide', p)
+        awful.hooks.user.call('tabdisplay', cl)
     end
 end
 
@@ -73,20 +60,20 @@ end
 -- @param tabindex The tab index.
 -- @param cl The client
 -- @return The key.
-function P.tabindex_check(tabindex, cl)
-    local c = cl or client.focus_get()
-    return P.findkey(tabbed[tabindex], c)
+function tabindex_check(tabindex, cl)
+    local c = cl or capi.client.focus_get()
+    return findkey(tabbed[tabindex], c)
 end
 
 --- Find the tab index or return nil if not tabbed.
 -- @param cl The client to search.
 -- @return The tab index.
-function P.tabindex_get(cl)
-    local c = cl or client.focus_get()
+function tabindex_get(cl)
+    local c = cl or capi.client.focus_get()
 
     for tabindex, tabdisplay in pairs(tabbed) do
         -- Loop through all tab displays
-        local me = P.tabindex_check(tabindex, c)
+        local me = tabindex_check(tabindex, c)
 
         if me ~= nil then
             return tabindex 
@@ -99,7 +86,7 @@ end
 --- Get all clients on a tabbed display.
 -- @param tabindex The tab index.
 -- @return All tabbed clients.
-function P.clients_get(tabindex)
+function clients_get(tabindex)
     if tabbed[tabindex] == nil then return nil end
     return tabbed[tabindex][2]
 end
@@ -107,7 +94,7 @@ end
 --- Get the displayed client on a tabbed display.
 -- @param tabindex The tab index.
 -- @return The displayed client.
-function P.displayed_get(tabindex)
+function displayed_get(tabindex)
     if tabbed[tabindex] == nil then return nil end
     return tabbed[tabindex][1]
 end
@@ -116,7 +103,7 @@ end
 -- @param tabindex The tab index.
 -- @param pos The position in the tab.
 -- @return The client at the given position.
-function P.position_get(tabindex, pos)
+function position_get(tabindex, pos)
     if tabbed[tabindex] == nil then return nil end
     return tabbed[tabindex][2][pos]
 end
@@ -125,10 +112,10 @@ end
 -- @param tabindex The tab index.
 -- @param cl The current client.
 -- @return The next client.
-function P.next(tabindex, cl)
+function next(tabindex, cl)
     local c = cl or tabbed[tabindex][1]
 
-    local i = P.findkey(tabbed[tabindex][2], c)
+    local i = findkey(tabbed[tabindex][2], c)
 
     if i == nil then
         return nil
@@ -145,10 +132,10 @@ end
 -- @param tabindex The tab index.
 -- @param cl The current client.
 -- @return The previous client.
-function P.prev(tabindex, cl)
+function prev(tabindex, cl)
     local c = cl or tabbed[tabindex][1]
 
-    local i = P.findkey(tabbed[tabindex][2], c)
+    local i = findkey(tabbed[tabindex][2], c)
 
     if i == nil then
         return nil
@@ -164,16 +151,16 @@ end
 --- Remove a client from a tabbed display.
 -- @param cl The client to remove.
 -- @return True if the client has been untabbed.
-function P.untab(cl)
-    local c = cl or client.focus_get()
-    local tabindex = P.tabindex_get(c)
+function untab(cl)
+    local c = cl or capi.client.focus_get()
+    local tabindex = tabindex_get(c)
 
     if tabindex == nil then return false end
 
-    local cindex = P.findkey(tabbed[tabindex][2], c)
+    local cindex = findkey(tabbed[tabindex][2], c)
 
     if tabbed[tabindex][1] == c then
-        P.display(tabindex, P.next(tabindex, c))
+        display(tabindex, P.next(tabindex, c))
     end
 
     table.remove(tabbed[tabindex][2], cindex)
@@ -184,15 +171,15 @@ function P.untab(cl)
     end
 
     c.hide = false
-    awful.hooks.userhook_call('untabbed', {c})
+    awful.hooks.user.call('untabbed', c)
 end
 
 --- Untab all clients in a tabbed display.
 -- @param tabindex The tab index.
-function P.untab_all(tabindex)
+function untab_all(tabindex)
     for i,c in pairs(tabbed[tabindex][2]) do
         c.hide = false
-        awful.hooks.userhook_call('untabbed', {c})
+        awful.hooks.user.call('untabbed', c)
     end
 
     if tabbed[tabindex] ~= nil then
@@ -203,8 +190,8 @@ end
 --- Create a new tabbed display with client as the master.
 -- @param cl The client to set into the tab, focused one otherwise.
 -- @return The created tab index.
-function P.tab_create(cl)
-    local c = cl or client.focus_get()
+function tab_create(cl)
+    local c = cl or capi.client.focus_get()
 
     if not c then return end
 
@@ -213,35 +200,35 @@ function P.tab_create(cl)
         { c } -- List of windows in tabbed display
     })
 
-    awful.hooks.userhook_call('tabbed', {c})
-    return P.tabindex_get(c)
+    awful.hooks.user.call('tabbed', c)
+    return tabindex_get(c)
 end
 
 --- Add a client to a tabbed display.
 -- @param tabindex The tab index.
 -- @param cl The client to add, or the focused one otherwise.
-function P.tab(tabindex, cl)
-    local c = cl or client.focus_get()
+function tab(tabindex, cl)
+    local c = cl or capi.client.focus_get()
 
     if tabbed[tabindex] ~= nil then
-        local x = P.tabindex_get(c)
+        local x = tabindex_get(c)
 
         if x == nil then
             -- Add untabbed client to tabindex
             table.insert(tabbed[tabindex][2], c)
-            P.display(tabindex, c)
+            display(tabindex, c)
 
-            awful.hooks.userhook_call('tabbed', {c})
+            awful.hooks.user.call('tabbed', c)
         elseif x ~= tabindex then
             -- Merge two tabbed views
             local cc = tabbed[tabindex][1]
-            local clients = P.clients_get(x)
-            P.untab_all(x)
+            local clients = clients_get(x)
+            untab_all(x)
 
-            tabindex = P.tabindex_get(cc)
+            tabindex = tabindex_get(cc)
 
             for i,b in pairs(clients) do
-                P.tab(tabindex, b)
+                tab(tabindex, b)
             end
         end
     end
@@ -249,20 +236,18 @@ end
 
 --- Start autotabbing, this automatically tabs new clients when the current
 -- client is tabbed.
-function P.autotab_start()
-    awful.hooks.manage(function (c)
-        local sel = client.focus_get()
-        local index = P.tabindex_get(sel)
+function autotab_start()
+    awful.hooks.manage.register(function (c)
+        local sel = capi.client.focus_get()
+        local index = tabindex_get(sel)
 
         if index ~= nil then
             -- Currently focussed client is tabbed, 
             -- add the new window to the tabbed display
-            P.tab(index, c)
+            tab(index, c)
         end
     end)
 end
 
 -- Set up hook so we don't leave lost hidden clients
-awful.hooks.unmanage(P.untab)
-
-return P
+awful.hooks.unmanage.register(untab)
