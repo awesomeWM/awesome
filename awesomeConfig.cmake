1e51843fa607aaf27c549aee893fb3e690138198
Julien Danjou <julien@danjou.info>
2008-08-29 11:35:52 +0200

    build: change codename
    Signed-off-by: Julien Danjou <julien@danjou.info>

--- awesomeConfig.cmake
+++ awesomeConfig.cmake
@@ -5,7 +5,7 @@ set(PROJECT_AWECLIENT_NAME awesome-client)
 # `git describe` later.
 set(VERSION devel)
 
-set(CODENAME "Scared Of Girls")
+set(CODENAME "Marshals Are Dead")
 
 project(${PROJECT_AWE_NAME} C)
 
