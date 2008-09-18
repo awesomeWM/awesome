57a537e168d5e7f75c3e1fead439d171cea8dc8a
Julien Danjou <julien@danjou.info>
2008-09-18 16:12:42 +0200

    change codename
    Signed-off-by: Julien Danjou <julien@danjou.info>

--- awesomeConfig.cmake
+++ awesomeConfig.cmake
@@ -5,7 +5,7 @@ set(PROJECT_AWECLIENT_NAME awesome-client)
 # `git describe` later.
 set(VERSION devel)
 
-set(CODENAME "Elect The Dead")
+set(CODENAME "Fake Plastic Trees")
 
 project(${PROJECT_AWE_NAME} C)
 
