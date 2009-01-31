2a2ce6e83ead5995c7852f0f6831817de05eb508
Julien Danjou <julien@danjou.info>
2009-01-31 16:03:06 +0100

    client: switch to xcb-icccm 0.3.3
    Signed-off-by: Julien Danjou <julien@danjou.info>

--- client.c
+++ client.c
@@ -1649,7 +1649,7 @@ luaA_client_index(lua_State *L)
                                    xcb_get_wm_class_unchecked(globalconf.connection, (*c)->win),
                                    &hint, NULL))
              return 0;
-        lua_pushstring(L, hint.class);
+        lua_pushstring(L, hint.class_name);
         xcb_get_wm_class_reply_wipe(&hint);
         break;
       case A_TK_INSTANCE:
@@ -1657,7 +1657,7 @@ luaA_client_index(lua_State *L)
                                    xcb_get_wm_class_unchecked(globalconf.connection, (*c)->win),
                                    &hint, NULL))
             return 0;
-        lua_pushstring(L, hint.name);
+        lua_pushstring(L, hint.instance_name);
         xcb_get_wm_class_reply_wipe(&hint);
         break;
       case A_TK_ROLE:
