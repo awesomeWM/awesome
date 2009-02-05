2b9bd3f26dac201de9e0ee1e780e1879f20c8c8b
Julien Danjou <julien@danjou.info>
2009-02-05 15:39:52 +0100

    change codename
    Signed-off-by: Julien Danjou <julien@danjou.info>

--- awesomeConfig.cmake
+++ awesomeConfig.cmake
@@ -5,7 +5,7 @@ set(PROJECT_AWECLIENT_NAME awesome-client)
 # `git describe` later.
 set(VERSION devel)
 
-set(CODENAME "Ring My Bell")
+set(CODENAME "Nobody Came")
 
 project(${PROJECT_AWE_NAME} C)
 
