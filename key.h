bea4aa7f601d3dd42f6e552093c7a8336c2bec6c
Julien Danjou <julien@danjou.info>
2009-04-29 10:39:06 +0200

    key: move keysym resolution to key
    Signed-off-by: Julien Danjou <julien@danjou.info>

--- key.h
+++ key.h
@@ -42,6 +42,7 @@ typedef struct keyb_t
 
 ARRAY_TYPE(keyb_t *, key)
 
+bool key_press_lookup_string(uint16_t, xcb_keycode_t, char *, ssize_t, xcb_keysym_t *);
 xcb_keysym_t key_getkeysym(xcb_keycode_t, uint16_t);
 
 void luaA_key_array_set(lua_State *, int, key_array_t *);
