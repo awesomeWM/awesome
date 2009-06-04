073c65841864542211fe11f4bccc9ffdc93e0aec
Julien Danjou <julien@danjou.info>
2009-06-04 10:58:26 +0200

    draw: remove area_array_t type
    Not used anymore.
    
    Signed-off-by: Julien Danjou <julien@danjou.info>

--- draw.h
+++ draw.h
@@ -77,8 +77,6 @@ struct area_t
     uint16_t height;
 };
 
-DO_ARRAY(area_t, area, DO_NOTHING)
-
 #define AREA_LEFT(a)    ((a).x)
 #define AREA_TOP(a)     ((a).y)
 #define AREA_RIGHT(a)   ((a).x + (a).width)
