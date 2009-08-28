ee171f89db80e13294cdc66999224cf90de86913
Julien Danjou <julien@danjou.info>
2009-08-28 15:42:11 +0200

    luadoc: add selection documentation
    Signed-off-by: Julien Danjou <julien@danjou.info>

---
+++ luadoc/selection.lua
@@ -0,0 +1,10 @@
+--- awesome selection (clipboard) API
+-- @author Julien Danjou &lt;julien@danjou.info&gt;
+-- @copyright 2008-2009 Julien Danjou
+module("selection")
+
+--- Get the selection (clipboard) content.
+-- @param -
+-- @return A string with the selection (clipboard) content.
+-- @name selection
+-- @class function
