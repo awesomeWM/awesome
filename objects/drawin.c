c6658742ff98f86b47ca41865981284efa645296
Uli Schlachter <psychon@znc.in>
2012-10-26 21:11:27 +0200

    drawin: Update the drawable's geometry on move
    We only need to update a drawin's drawing related resources when its size
    changes. Thus, when a drawin is just moved, drawin_update_drawing() is not
    called. However, this function was used for telling the drawin's drawable about
    its geometry. This means that the drawable had an outdated idea about what its
    geometry was.
    
    Fix this by making sure to always call drawable_set_geometry().
    
    Thanks to Javafant for reporting this bug on IRC.
    
    Signed-off-by: Uli Schlachter <psychon@znc.in>

--- objects/drawin.c
+++ objects/drawin.c
@@ -197,6 +197,12 @@ drawin_moveresize(lua_State *L, int udx, area_t geometry)
 
     if(mask_vals & (XCB_CONFIG_WINDOW_WIDTH | XCB_CONFIG_WINDOW_HEIGHT))
         drawin_update_drawing(w, udx);
+    else {
+        /* We still have to set x/y */
+        luaA_object_push_item(L, udx, w->drawable);
+        drawable_set_geometry(w->drawable, -1, w->geometry);
+        lua_pop(L, 1);
+    }
 
     /* Activate BMA */
     client_ignore_enterleave_events();
