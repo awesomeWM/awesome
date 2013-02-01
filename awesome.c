03759b48478eb716af8b62e01ef3b5140c582f58
Uli Schlachter <psychon@znc.in>
2013-02-01 23:06:08 +0100

    Fix keyboard layouts
    Commit 08ca70c00d06c6 added support for Xlib cursor themes by making awesome use
    Xlib for its X11 connection and for setting cursor icons. Everything else was
    still done through xcb.
    
    However, an unexpected side effect of this is that Xlib enabled the XKB
    extension without asking us. This changes the way that keyboard events are
    reported which causes problems with various keyboard events.
    
    (Normally, Xlib hides these things by silently fixing up the events that it
    hands out to applications. However, since we bypassed Xlib via xcb...)
    
    The fix is simple: Luckily Xlib provides a function for disabling its XKB
    support which is just what we need.
    
    Signed-off-by: Uli Schlachter <psychon@znc.in>

--- awesome.c
+++ awesome.c
@@ -34,6 +34,7 @@
 #include <xcb/shape.h>
 
 #include <X11/Xlib-xcb.h>
+#include <X11/XKBlib.h>
 
 #include <glib-unix.h>
 
@@ -368,6 +369,9 @@ main(int argc, char **argv)
     struct sigaction sa = { .sa_handler = signal_fatal, .sa_flags = 0 };
     sigemptyset(&sa.sa_mask);
     sigaction(SIGSEGV, &sa, 0);
+
+    /* XLib sucks */
+    XkbIgnoreExtension(True);
 
     /* X stuff */
     globalconf.display = XOpenDisplay(NULL);
