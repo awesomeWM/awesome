ac50f4b4121c84060bde9457d6cd3b02d6328271
memeplex <carlosjosepita@gmail.com>
2014-08-18 02:32:11 -0300

    Fix for FS#1293
    FS#1293 - Systray won't show with margin layout
    
    https://awesome.naquadah.org/bugs/index.php?do=details&task_id=1293&project=1&order=dateopened&sort=desc

--- systray.c
+++ systray.c
@@ -323,6 +323,10 @@ systray_update(int base_size, bool horizontal, bool reverse, int spacing)
 int
 luaA_systray(lua_State *L)
 {
+
+    if(globalconf.systray.parent == NULL)
+        systray_register();
+
     if(lua_gettop(L) != 0)
     {
         size_t bg_len;
@@ -343,9 +347,6 @@ luaA_systray(lua_State *L)
                                          globalconf.systray.window,
                                          XCB_CW_BACK_PIXEL, config_back);
         }
-
-        if(globalconf.systray.parent == NULL)
-            systray_register();
 
         if(globalconf.systray.parent != w)
             xcb_reparent_window(globalconf.connection,
