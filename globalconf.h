df3e89ad5c952817db936691acde83b893dd4223
Uli Schlachter <psychon@znc.in>
2014-08-20 11:40:50 +0200

    systray: Only intern the atom once
    Let's just save the systray atom and keep it around. Why should we redo this
    every time this atom is needed?
    
    Signed-off-by: Uli Schlachter <psychon@znc.in>

--- globalconf.h
+++ globalconf.h
@@ -119,6 +119,8 @@ typedef struct
     struct
     {
         xcb_window_t window;
+        /** Atom for _NET_SYSTEM_TRAY_%d */
+        xcb_atom_t atom;
         /** Systray window parent */
         drawin_t *parent;
     } systray;
