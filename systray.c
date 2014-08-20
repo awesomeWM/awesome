0face4fea7882e74791fae653dd7e1716373c1b7
Uli Schlachter <psychon@znc.in>
2014-08-20 11:47:47 +0200

    systray: Only register/unregister when needed
    Signed-off-by: Uli Schlachter <psychon@znc.in>

--- systray.c
+++ systray.c
@@ -74,6 +74,11 @@ systray_register(void)
     xcb_client_message_event_t ev;
     xcb_screen_t *xscreen = globalconf.screen;
 
+    if(globalconf.systray.registered)
+        return;
+
+    globalconf.systray.registered = true;
+
     /* Fill event */
     p_clear(&ev, 1);
     ev.response_type = XCB_CLIENT_MESSAGE;
@@ -98,6 +103,11 @@ systray_register(void)
 void
 systray_cleanup(void)
 {
+    if(!globalconf.systray.registered)
+        return;
+
+    globalconf.systray.registered = false;
+
     xcb_set_selection_owner(globalconf.connection,
                             XCB_NONE,
                             globalconf.systray.atom,
@@ -297,9 +307,7 @@ systray_update(int base_size, bool horizontal, bool reverse, int spacing)
 int
 luaA_systray(lua_State *L)
 {
-
-    if(globalconf.systray.parent == NULL)
-        systray_register();
+    systray_register();
 
     if(lua_gettop(L) != 0)
     {
