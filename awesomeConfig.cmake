3113fee2eeccea08ef0918e0d1132391318e3c17
Uli Schlachter <psychon@znc.in>
2015-01-10 20:36:35 +0100

    change codename
    Signed-off-by: Uli Schlachter <psychon@znc.in>

--- awesomeConfig.cmake
+++ awesomeConfig.cmake
@@ -4,7 +4,7 @@ set(PROJECT_AWE_NAME awesome)
 # `git describe` later.
 set(VERSION devel)
 
-set(CODENAME "Kansas City Shuffle")
+set(CODENAME "For Those About To Rock")
 
 project(${PROJECT_AWE_NAME} C)
 
