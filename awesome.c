awesomeWM/awesome

commit 90fde1393f697ca0d7201e385293a5ac4778d136

awful.tag: Use delayed_call to call withcurrent on manage
This way this function runs after everything else did its job. The client will
only get assigned a tag after everything else had a chance to do so.

Thanks to this, awful.rules no longer has to disconnect the call to withcurrent.

Signed-off-by: Uli Schlachter <psychon@znc.in>

lib/awful/rules.lua.in
@@ -224,11 +224,6 @@ function rules.execute(c, props, callbacks)
         end
     end
 
-    -- If untagged, stick the client on the current one.
-    if #c:tags() == 0 then
-        atag.withcurrent(c)
-    end
-
     -- Apply all callbacks.
     if callbacks then
         for i, callback in pairs(callbacks) do
@@ -244,7 +239,6 @@ function rules.execute(c, props, callbacks)
 end
 
 client.connect_signal("manage", rules.apply)
-client.disconnect_signal("manage", atag.withcurrent)
 
 return rules
 

lib/awful/tag.lua.in
@@ -6,6 +6,7 @@
 
 -- Grab environment we need
 local util = require("awful.util")
+local timer = require("gears.timer")
 local tostring = tostring
 local pairs = pairs
 local ipairs = ipairs
@@ -629,7 +630,9 @@ capi.client.connect_signal("manage", function(c)
     c:connect_signal("property::screen", tag.withcurrent)
 end)
 
-capi.client.connect_signal("manage", tag.withcurrent)
+capi.client.connect_signal("manage", function(c)
+    timer.delayed_call(tag.withcurrent, c)
+end)
 capi.tag.connect_signal("request::select", tag.viewonly)
 
 capi.tag.add_signal("property::hide")

commit 6963ede3dca9d45e6813b4a099ffe8f8fa2dd46e

awful.rules.execute: use delayed_call for emitting request::activate
This is a followup to commit 90fde13, which caused the client not to be
visible during the request::activate callback.

This requires an initial call to `awesome_refresh` before the main loop
to get `awesome.startup` right.

Ref: https://github.com/awesomeWM/awesome/commit/90fde139#commitcomment-9308225

awesome.c
@@ -545,6 +545,10 @@ main(int argc, char **argv)
     g_main_context_set_poll_func(g_main_context_default(), &a_glib_poll);
     gettimeofday(&last_wakeup, NULL);
 
+    /* Do all deferred work now once outside of the loop to get awesome.startup
+     * right. */
+    awesome_refresh();
+
     /* main event loop */
     globalconf.loop = g_main_loop_new(NULL, FALSE);
     g_main_loop_run(globalconf.loop);

lib/awful/rules.lua.in
@@ -12,6 +12,7 @@ local ipairs = ipairs
 local pairs = pairs
 local aclient = require("awful.client")
 local atag = require("awful.tag")
+local timer = require("gears.timer")
 
 --- Apply rules to clients at startup.
 -- awful.rules
@@ -234,7 +235,10 @@ function rules.execute(c, props, callbacks)
     -- Do this at last so we do not erase things done by the focus
     -- signal.
     if props.focus and (type(props.focus) ~= "function" or props.focus(c)) then
-        c:emit_signal('request::activate',"rules")
+        local cb = function(c)
+            c:emit_signal('request::activate', "rules")
+        end
+        timer.delayed_call(cb, c)
     end
 end
 