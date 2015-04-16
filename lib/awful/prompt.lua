ed75702b511b89681b270dfbca96db460a137f14
Daniel Hahler <git@thequod.de>
2015-04-16 02:20:35 +0200

    Revert "awful.prompt.run: do not reset completion for Shift etc"
    This reverts commit 7ef34ce7b31f7c24b25475d612ba0f6bc3727339.

--- lib/awful/prompt.lua
+++ lib/awful/prompt.lua
@@ -539,10 +539,9 @@ function prompt.run(args, textbox, exe_callback, completion_callback, history_pa
         else
             if completion_callback then
                 if key == "Tab" or key == "ISO_Left_Tab" then
-                    if key == "ISO_Left_Tab" then  -- Shift-Tab
+                    if key == "ISO_Left_Tab" then
                         if ncomp == 1 then return end
                         if ncomp == 2 then
-                            -- Restore previous command.
                             command = command_before_comp
                             textbox:set_font(font)
                             textbox:set_markup(prompt_text_with_cursor{
@@ -566,8 +565,7 @@ function prompt.run(args, textbox, exe_callback, completion_callback, history_pa
                         exec(exe_callback)
                         return
                     end
-                elseif key:wlen() == 1 then
-                    -- No control char, i.e. Shift_L or Shift_R.
+                else
                     ncomp = 1
                 end
             end
