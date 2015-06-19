581ccc80ad8a3b2d8f7ac5608ee1d18906bfe258
Daniel Hahler <git@thequod.de>
2015-06-20 01:56:05 +0200

    cmake: s/ESCAPE_QUOTE/ESCAPE_QUOTES
    This appears to be the correct name for 2.8.12 [1] already, but CMake
    3.2.3 complains loud(er) now.
    
    1: http://www.cmake.org/cmake/help/v2.8.12/cmake.html#command:configure_file

--- awesomeConfig.cmake
+++ awesomeConfig.cmake
@@ -296,7 +296,7 @@ macro(a_configure_file file)
     message(STATUS "Configuring ${outfile}")
     configure_file(${SOURCE_DIR}/${file}
                    ${BUILD_DIR}/${outfile}
-                   ESCAPE_QUOTE
+                   ESCAPE_QUOTES
                    @ONLY)
 endmacro()
 
