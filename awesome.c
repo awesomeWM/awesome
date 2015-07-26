86e242b9831a968285da0dce1fa65aef4bf206a2
Uli Schlachter <psychon@znc.in>
2015-07-26 15:59:34 +0200

    Keep stacking order across restarts
    ReparentWindow puts the window at the top of the stacking order. Thus, we have
    to reparent clients back to the root window in the stacking order.
    
    Signed-off-by: Uli Schlachter <psychon@znc.in>

--- awesome.c
+++ awesome.c
@@ -78,8 +78,8 @@ awesome_atexit(bool restart)
     lua_pushboolean(L, restart);
     signal_object_emit(L, &global_signals, "exit", 1);
 
-    /* Move clients where we want them to be */
-    foreach(c, globalconf.clients)
+    /* Move clients where we want them to be and keep the stacking order intact */
+    foreach(c, globalconf.stack)
     {
         xcb_reparent_window(globalconf.connection, (*c)->window, globalconf.screen->root,
                 (*c)->geometry.x, (*c)->geometry.y);
