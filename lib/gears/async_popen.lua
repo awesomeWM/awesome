cfc1189ddfb3f144417acea48aeb930ebd413ccd
Benjamin Petrenko <benjamin.petrenko@gmail.com>
2015-07-28 12:16:49 +0300

    Add gears.async_popen

---
+++ lib/gears/async_popen.lua
@@ -0,0 +1,40 @@
+---------------------------------------------------------------------------
+-- @release @AWESOME_VERSION@
+-- @module gears.async_popen
+---------------------------------------------------------------------------
+
+local setmetatable = setmetatable
+local util = require("awful.util")
+
+local async_popen = { mt = {} }
+
+-- Maps temporary filenames to callbacks
+async_popen.callbacks = {}
+
+--- Run a command asynchronously.
+-- @param command The command to run
+-- @param callback The function to be called when the command exits. A filehandle of the output file will be passed to it.
+function async_popen.async_popen(command, callback)
+    local tmpfname = os.tmpname()
+    async_popen.callbacks[tmpfname] = callback
+    util.spawn(string.format("bash -c '%s > %s; echo \"async_popen.process_output(\\\"%s\\\")\" | awesome-client' 2> /dev/null",
+                             string.gsub(command, "'", "'\\''"),
+                             tmpfname,
+                             tmpfname),
+               false)
+end
+
+function async_popen.process_output(tmpfname)
+    if async_popen.callbacks[tmpfname] then
+        local fh = io.open(tmpfname, "r")
+        async_popen.callbacks[tmpfname](fh)
+    end
+end
+
+function async_popen.mt:__call(...)
+    return async_popen.async_popen(...)
+end
+
+return setmetatable(async_popen, async_popen.mt)
+
+-- vim: filetype=lua:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:textwidth=80
