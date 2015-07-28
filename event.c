1118ec44391b70e22dadcbc5f8b2397bdb79bafb
Daniel Hahler <git@thequod.de>
2015-07-28 18:52:41 +0200

    Do not call `xcb_ungrab_key` twice (before `xwindow_grabkeys`)
    It gets called in `xwindow_grabkeys` always:
    
        xwindow_grabkeys(xcb_window_t win, key_array_t *keys)
        {
            /* Ungrab everything first */
            xcb_ungrab_key(globalconf.connection, XCB_GRAB_ANY, win, XCB_BUTTON_MASK_ANY);
    
    Closes https://github.com/awesomeWM/awesome/pull/366.

--- event.c
+++ event.c
@@ -801,14 +801,11 @@ event_handle_mappingnotify(xcb_mapping_notify_event_t *ev)
 
         /* regrab everything */
         xcb_screen_t *s = globalconf.screen;
-        /* yes XCB_BUTTON_MASK_ANY is also for grab_key even if it's look weird */
-        xcb_ungrab_key(globalconf.connection, XCB_GRAB_ANY, s->root, XCB_BUTTON_MASK_ANY);
         xwindow_grabkeys(s->root, &globalconf.keys);
 
         foreach(_c, globalconf.clients)
         {
             client_t *c = *_c;
-            xcb_ungrab_key(globalconf.connection, XCB_GRAB_ANY, c->frame_window, XCB_BUTTON_MASK_ANY);
             xwindow_grabkeys(c->frame_window, &c->keys);
         }
     }
