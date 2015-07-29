70120ae4adb7bad84b163f22245cc763aa91f8ca
Uli Schlachter <psychon@znc.in>
2015-07-29 21:25:51 +0200

    fixup!
    Signed-off-by: Uli Schlachter <psychon@znc.in>

--- spec/preload.lua
+++ spec/preload.lua
@@ -1,4 +1,4 @@
 -- This script is given to Busted via the --helper argument. Modules loaded here
 -- won't be cleared and reloaded by Busted. This is needed for lgi because lgi
--- is not safe to reload and yet Busted makes us try to do this.
+-- is not safe to reload and yet Busted manages to do this.
 require("lgi")
