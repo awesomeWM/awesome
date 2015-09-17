42e00819584c9cdb6f79fca317ebdb0d36902f10
Uli Schlachter <psychon@znc.in>
2015-09-17 16:32:56 +0200

    Fix enter/leave events on titlebars
    This adds a global variable that tracks the drawable that is currently under the
    mouse cursor. This new variable is then used so that we get consistent behavior
    for enter/leave signals. Such signals are now also generated when a MotionNotify
    event tells us that the pointer is now in a different titlebar.
    
    Before this, it was possible that we did not generate a leave event on a
    titlebar since the LeaveNotify contains the cursor position after the leave and
    we did not manage to figure out which titlebar was left.
    
    Signed-off-by: Uli Schlachter <psychon@znc.in>

--- globalconf.h
+++ globalconf.h
@@ -108,6 +108,8 @@ typedef struct
     int keygrabber;
     /** The mouse pointer grabber function */
     int mousegrabber;
+    /** The drawable that currently contains the pointer */
+    drawable_t *drawable_under_mouse;
     /** Input focus information */
     struct
     {
