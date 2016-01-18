e6151735521c785e14f61cdb9f6f8a0434e42ea9
Emmanuel Lepage Vallee <elv1313@gmail.com>
2016-01-17 23:23:50 -0500

    layout.fixed: Add methods to manipulate the layout after creation
    Until now, this layout was "append only". There was no official
    APIs to remove, replace, insert and swap widgets. This is fine
    for the usual wibox + sensors widget used by the majority of
    users, but lack flexibility necessary to use the layout system
    to place dynamic elements such as clients.
    
    The methods introduced by this commit are also recursive. This
    allow widgets to be decorated, wrapped and splitted without
    having to add boilerplate code everywhere.

--- lib/wibox/layout/flex.lua
+++ lib/wibox/layout/flex.lua
@@ -37,6 +37,27 @@ local flex = {}
 -- @name add
 -- @class function
 
+--- Set a widget at a specific index, replace the current one
+-- @tparam number index A widget or a widget index
+-- @param widget2 The widget to take the place of the first one
+-- @treturn boolean If the operation is successful
+-- @name set
+-- @class function
+
+--- Remove a widget from the layout
+-- @tparam index The widget index to remove
+-- @treturn boolean index If the operation is successful
+-- @name remove
+-- @class function
+
+--- Remove one or more widgets from the layout
+-- The last parameter can be a boolean, forcing a recursive seach of the
+-- widget(s) to remove.
+-- @param widget ... Widgets that should be removed (must at least be one)
+-- @treturn boolean If the operation is successful
+-- @name remove_widgets
+-- @class function
+
 --- Fit the fixed layout into the given space
 -- @param layout The layout you are modifying.
 -- @param context The context in which we are fit.
@@ -48,6 +69,37 @@ local flex = {}
 --- Reset a fixed layout. This removes all widgets from the layout.
 -- @param layout The layout you are modifying.
 -- @name reset
+-- @class function
+
+--- Replace the first instance of `widget` in the layout with `widget2`
+-- @param widget The widget to replace
+-- @param widget2 The widget to replace `widget` with
+-- @tparam[opt=false] boolean recursive Digg in all compatible layouts to find the widget.
+-- @treturn boolean If the operation is successful
+-- @name replace_widget
+-- @class function
+
+--- Swap 2 widgets in a layout
+-- @tparam number index1 The first widget index
+-- @tparam number index2 The second widget index
+-- @treturn boolean If the operation is successful
+-- @name swap
+-- @class function
+
+--- Swap 2 widgets in a layout
+-- If widget1 is present multiple time, only the first instance is swapped
+-- @param widget1 The first widget
+-- @param widget2 The second widget
+-- @tparam[opt=false] boolean recursive Digg in all compatible layouts to find the widget.
+-- @treturn boolean If the operation is successful
+-- @name swap_widgets
+-- @class function
+
+--- Insert a new widget in the layout at position `index`
+-- @tparam number index The position
+-- @param widget The widget
+-- @treturn boolean If the operation is successful
+-- @name insert
 -- @class function
 
 --- Layout a flex layout. Each widget gets an equal share of the available space.
