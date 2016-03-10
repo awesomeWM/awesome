ac15475c5879805ae5a9fd32b031209593398eda
Emmanuel Lepage Vallee <elv1313@gmail.com>
2016-03-09 23:44:00 -0500

    gears.shape: Add unit tests / examples / SVG generator

---
+++ tests/shape/isosceles_triangle.lua
@@ -0,0 +1,10 @@
+local shape,cr,show = ... --DOC_HIDE
+
+shape.isosceles_triangle(cr, 70, 70)
+show(cr) --DOC_HIDE
+ 
+shape.isosceles_triangle(cr,20,70)
+show(cr) --DOC_HIDE
+ 
+shape.transform(shape.isosceles_triangle) : rotate_at(35, 35, math.pi/2)(cr,70,70)
+show(cr) --DOC_HIDE
---
+++ tests/shape/parallelogram.lua
@@ -0,0 +1,10 @@
+local shape,cr,show = ... --DOC_HIDE
+
+shape.parallelogram(cr, 70, 70)
+show(cr) --DOC_HIDE
+ 
+shape.parallelogram(cr,70,20)
+show(cr) --DOC_HIDE
+ 
+shape.transform(shape.parallelogram) : scale(0.5, 1)(cr,70,70)
+show(cr) --DOC_HIDE
