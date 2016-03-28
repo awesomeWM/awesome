ef5ec333cc590733f2231b513d14a323421ad59d
Emmanuel Lepage Vallee <elv1313@gmail.com>
2016-03-28 02:14:09 -0400

    tests: Test the remaining existing `awful.placement` methods.
    Notes that some asserts are missing because the tests fail. This
    will be fixed later. It is not a regression.

---
+++ tests/examples/awful/placement/next_to_mouse.lua
@@ -0,0 +1,8 @@
+screen[1]._resize {width = 128, height = 96} --DOC_HIDE
+local awful = {placement = require("awful.placement")} --DOC_HIDE
+client.gen_fake {x = 10, y = 10, width=40, height=30} --DOC_HIDE
+
+mouse.coords {x= 50, y=50} --DOC_HIDE
+mouse.push_history() --DOC_HIDE
+
+awful.placement.next_to_mouse(client.focus)
---
+++ tests/examples/awful/placement/no_overlap.lua
@@ -0,0 +1,26 @@
+screen[1]._resize {width = 128, height = 96} --DOC_HIDE
+screen._add_screen {x = 140, y = 0  , width = 128, height = 96} --DOC_HIDE
+screen._add_screen {x = 0  , y = 110, width = 128, height = 96} --DOC_HIDE
+screen._add_screen {x = 140, y = 110, width = 128, height = 96} --DOC_HIDE
+local awful = {placement = require("awful.placement")} --DOC_HIDE
+local beautiful = require("beautiful") --DOC_HIDE
+
+
+client.gen_fake {x = 10, y = 10, width=40, height=30} --DOC_HIDE
+client.gen_fake {x = 80, y = 55, width=40, height=30} --DOC_HIDE
+client.gen_fake {x = 0, y = 0, width=40, height=50, color=beautiful.bg_highlight} --DOC_HIDE
+client.focus:_hide() --DOC_HIDE
+
+awful.placement.no_overlap(client.focus)
+
+local x,y = screen[4].geometry.x, screen[4].geometry.y
+client.gen_fake {x = x+10, y = y+10, width=40, height=30} --DOC_HIDE
+client.gen_fake {x = x+80, y = y+10, width=40, height=30} --DOC_HIDE
+client.gen_fake {x = x+10, y = y+55, width=40, height=30} --DOC_HIDE
+client.gen_fake {x = x+80, y = y+55, width=40, height=30} --DOC_HIDE
+client.gen_fake {x = x+0, y = y+0, width=40, height=50, color=beautiful.bg_highlight} --DOC_HIDE
+client.focus:_hide() --DOC_HIDE
+awful.placement.no_overlap(client.focus) --FIXME --DOC_HIDE
+
+--TODO maximized + no_overlap --DOC_HIDE
+--TODO add 9 clients with no_overlap on all of them --DOC_HIDE
