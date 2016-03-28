4bc3f04ec162ee9c1f671ec9e7850831d3da0daf
Emmanuel Lepage Vallee <elv1313@gmail.com>
2016-03-28 02:09:34 -0400

    tests: Test `awful.placement.maximize` aliases

---
+++ tests/examples/awful/placement/maximize_vertically.lua
@@ -0,0 +1,15 @@
+-- Vetically maximize the drawable in the parent area. --DOC_HEADER
+-- @tparam drawable d A drawable (like `client` or `wibox`) --DOC_HEADER
+-- @tparam[opt={}] table args Other arguments") --DOC_HEADER
+-- @name maximize_vertically --DOC_HEADER
+-- @class function --DOC_HEADER
+
+screen[1]._resize {width = 128, height = 96} --DOC_HIDE
+local placement = require("awful.placement") --DOC_HIDE
+
+local c = client.gen_fake {x = 45, y = 35, width=40, height=30} --DOC_HIDE
+placement.maximize_vertically(c)
+
+assert(c.height+2*c.border_width == screen[1].geometry.height)--DOC_HIDE
+assert(c.x == 45) --DOC_HIDE
+assert(c.width == 40) --DOC_HIDE
