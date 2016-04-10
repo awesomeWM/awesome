3f0483003c42c9f566cbc7d470e455fcfc7181da
Uli Schlachter <psychon@znc.in>
2016-04-10 11:21:37 +0200

    Default config: Use strings for tag names
    Tag names really are strings. Numbers just work accidentally since the C code
    uses luaL_checklstring() to access the tag name and this function silently
    converts numbers to strings.
    
    This also has a nice documentation effect, making it easier for people to figure
    out that they can change the name of a tag. Plus, with this the changes done by
    previous commits make more sense (specifying an awful.rules-rule that identifies
    a tag by name).
    
    Signed-off-by: Uli Schlachter <psychon@znc.in>

--- awesomerc.lua
+++ awesomerc.lua
@@ -102,7 +102,7 @@ end
 tags = {}
 awful.screen.connect_for_each_screen(function(s)
     -- Each screen has its own tag table.
-    tags[s] = awful.tag({ 1, 2, 3, 4, 5, 6, 7, 8, 9 }, s, awful.layout.layouts[1])
+    tags[s] = awful.tag({ "1", "2", "3", "4", "5", "6", "7", "8", "9" }, s, awful.layout.layouts[1])
 end)
 -- }}}
 
