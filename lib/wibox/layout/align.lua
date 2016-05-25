5ba7af2be20b49c10dc50260f29b099d6e19306f
Emmanuel Lepage Vallee <elv1313@gmail.com>
2016-05-25 15:02:54 -0400

    doc: Add an align layout default screenshot

--- lib/wibox/layout/align.lua
+++ lib/wibox/layout/align.lua
@@ -1,4 +1,6 @@
 ---------------------------------------------------------------------------
+--
+--@DOC_wibox_layout_defaults_align_EXAMPLE@
 -- @author Uli Schlachter
 -- @copyright 2010 Uli Schlachter
 -- @release @AWESOME_VERSION@
