a8505ed019e3513c34d4e4b31148d926870cd7bf
Emmanuel Lepage Vallee <elv1313@gmail.com>
2016-05-25 17:56:23 -0400

    doc: Add an imagebox widget default screenshot

--- lib/wibox/widget/imagebox.lua
+++ lib/wibox/widget/imagebox.lua
@@ -1,4 +1,6 @@
 ---------------------------------------------------------------------------
+--
+--@DOC_wibox_widget_defaults_imagebox_EXAMPLE@
 -- @author Uli Schlachter
 -- @copyright 2010 Uli Schlachter
 -- @release @AWESOME_VERSION@
---
+++ tests/examples/wibox/widget/defaults/imagebox.lua
@@ -0,0 +1,13 @@
+local parent    = ... --DOC_HIDE
+local wibox     = require( "wibox"     ) --DOC_HIDE
+local beautiful = require( "beautiful" ) --DOC_HIDE
+
+parent:add( --DOC_HIDE
+
+wibox.widget {
+    image  = beautiful.awesome_icon,
+    resize = false,
+    widget = wibox.widget.imagebox
+}
+
+) --DOC_HIDE
