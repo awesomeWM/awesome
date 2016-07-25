b49b859fac1fedf25b74e9d9cfd9ac2dfc470fbf
Emmanuel Lepage Vallee <elv1313@gmail.com>
2016-07-24 21:46:37 -0400

    tests: Use the new progressbar features in the default test.

--- tests/examples/wibox/widget/defaults/progressbar.lua
+++ tests/examples/wibox/widget/defaults/progressbar.lua
@@ -1,12 +1,19 @@
 local parent = ... --DOC_HIDE
 local wibox  = require("wibox") --DOC_HIDE
+local gears  = {shape = require("gears.shape") } --DOC_HIDE
+local beautiful = require("beautiful") --DOC_HIDE
 
 parent:add( --DOC_HIDE
 
 wibox.widget {
-    max_value = 1,
-    value     = 0.33,
-    widget    = wibox.widget.progressbar
+    max_value     = 1,
+    value         = 0.33,
+    forced_height = 20,
+    forced_width  = 100,
+    shape         = gears.shape.rounded_bar,
+    border_width  = 2,
+    border_color  = beautiful.border_color,
+    widget        = wibox.widget.progressbar,
 }
 
 ) --DOC_HIDE
