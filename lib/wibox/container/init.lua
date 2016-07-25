f483eb041a65f8598f5cb6a9c9fd50f5c4455883
Emmanuel Lepage Vallee <elv1313@gmail.com>
2016-07-25 00:15:42 -0400

    radialprogressbar: Upstream Elv13 round progressbar

--- lib/wibox/container/init.lua
+++ lib/wibox/container/init.lua
@@ -15,6 +15,7 @@ return setmetatable({
     constraint = require("wibox.container.constraint");
     scroll = require("wibox.container.scroll");
     background = require("wibox.container.background");
+    radialprogressbar = require("wibox.container.radialprogressbar");
 }, {__call = function(_, args) return base.make_widget_declarative(args) end})
 
 -- vim: filetype=lua:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:textwidth=80
