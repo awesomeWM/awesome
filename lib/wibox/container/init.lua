b7ee22e0359004bb1877bf2205dc03a10e5de039
Emmanuel Lepage Vallee <elv1313@gmail.com>
2016-08-04 17:43:38 -0400

    container: Add an 'Arc chart' container.
    A lot of conky config use this type of widgets. It looks very nice
    on thicker wiboxes.

--- lib/wibox/container/init.lua
+++ lib/wibox/container/init.lua
@@ -16,6 +16,7 @@ return setmetatable({
     scroll = require("wibox.container.scroll");
     background = require("wibox.container.background");
     radialprogressbar = require("wibox.container.radialprogressbar");
+    arcchart = require("wibox.container.arcchart");
 }, {__call = function(_, args) return base.make_widget_declarative(args) end})
 
 -- vim: filetype=lua:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:textwidth=80
