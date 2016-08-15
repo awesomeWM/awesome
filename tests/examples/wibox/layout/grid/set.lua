dc09d76d30fa7d05bc8c486664f74e3012d3353e
Emmanuel Lepage Vallee <elv1313@gmail.com>
2016-08-15 02:57:02 -0400

    tests: Test the grid layout.

---
+++ tests/examples/wibox/layout/grid/set.lua
@@ -0,0 +1,14 @@
+local generic_widget, generic_before_after = ... --DOC_HIDE_ALL
+local wibox = require("wibox")
+local beautiful = require("beautiful")
+
+local w = generic_before_after(wibox.layout.grid, {
+    column_count = 3,
+    row_count    = 2,
+    fill_space   = false,
+    spacing      = 10,
+}, 6, "set", {4, wibox.widget(
+    generic_widget("__new__",beautiful.bg_highlight)
+)})
+
+return w, w:fit({dpi=96}, 9999, 9999)
