2a976951ea5ab3bf2a337566c9d360ca3a2897b9
Emmanuel Lepage Vallee <elv1313@gmail.com>
2016-09-08 02:32:53 -0400

    tests: The the piechart widget.

---
+++ tests/examples/wibox/widget/piechart/border_width.lua
@@ -0,0 +1,26 @@
+--DOC_HIDE_ALL
+local parent    = ...
+local wibox     = require( "wibox"     )
+local beautiful = require( "beautiful" )
+
+local l = wibox.layout.fixed.horizontal()
+parent:add(l)
+
+for _, v in ipairs {0,1,3,5} do
+    l:add(wibox.widget {
+        data = {
+            ['L1'] = 100,
+            ['L2'] = 200,
+            ['L3'] = 300,
+        },
+        border_width = v,
+        forced_height = 50,
+        forced_width  = 100,
+        colors = {
+            beautiful.bg_normal,
+            beautiful.bg_highlight,
+            beautiful.border_color,
+        },
+        widget = wibox.widget.piechart
+    })
+end
