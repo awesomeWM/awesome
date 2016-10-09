01ea62a57fd06b9b06021234c5df0371dfa97031
Uli Schlachter <psychon@znc.in>
2016-10-09 17:12:33 +0200

    gears.shape: Simplify rotation of shapes
    This adds helper functions for rotating a shape left, right and by 180
    degrees.
    
    Signed-off-by: Uli Schlachter <psychon@znc.in>

---
+++ tests/examples/gears/shape/rotate_right.lua
@@ -0,0 +1,10 @@
+local shape,cr,show = ... --DOC_HIDE
+
+local original_shape = shape.arrow
+local rotated_shape = shape.rotate_right(original_shape)
+
+original_shape(cr, 70, 70)
+show(cr) --DOC_HIDE
+
+rotated_shape(cr, 70, 70)
+show(cr) --DOC_HIDE
