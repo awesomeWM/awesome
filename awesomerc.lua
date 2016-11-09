395984f5b3cfb323cd1b215001ede7b620409e15
Daniel Hahler <git@thequod.de>
2016-11-09 21:52:33 +0100

    fixup! Minor doc and code style fixes

--- awesomerc.lua
+++ awesomerc.lua
@@ -555,7 +555,7 @@ client.connect_signal("request::titlebars", function(c)
     }
 end)
 
--- Enable sloppy focus
+-- Enable sloppy focus, so that focus follows mouse.
 client.connect_signal("mouse::enter", function(c)
     if awful.layout.get(c.screen) ~= awful.layout.suit.magnifier
         and awful.client.focus.filter(c) then
