771f5a13c6841bf6ef2bafaa85b63ab935281e9d
Emmanuel Lepage Vallee <elv1313@gmail.com>
2016-12-01 19:34:46 -0500

    tests: Bump the timeout to 3 minutes.
    There we go again... When hitting a slow CI node, there is again
    timeouts when doing the multi-screen tests. As no solution to
    bug leading to this has been found, the only thing to do is
    increase the timeout.
    
    Hopefully this commit will be reverted soon.

--- tests/run.sh
+++ tests/run.sh
@@ -195,7 +195,7 @@ start_awesome() {
 # Count errors.
 errors=0
 # Seconds after when awesome gets killed.
-timeout_stale=120 # FIXME This should be no more than 60s
+timeout_stale=180 # FIXME This should be no more than 60s
 
 for f in $tests; do
     echo "== Running $f =="
