awesomeWM/awesome

commit 5e6a8932078e3352c972dfb811669f3b96cb3978

Replace various calls to warn() with luaA_warn()
luaA_warn() prints a Lua backtrace and thus generates more useful output. warn()
should only be used in awesome-internal places (e.g. receiving an error from the
X11 server).

Closes https://github.com/awesomeWM/awesome/pull/608.

Signed-off-by: Uli Schlachter <psychon@znc.in>

spawn.c
@@ -419,9 +419,7 @@ luaA_spawn(lua_State *L)
     if(!argv || !argv[0])
     {
         g_strfreev(argv);
-        /* push error on stack */
-        lua_pushfstring(L, "spawn: parse error: %s", error->message);
-        warn("%s", lua_tostring(L, -1));
+        luaA_warn(L, "spawn: parse error: %s", error->message);
         g_error_free(error);
         return 1;
     }
@@ -447,9 +445,7 @@ luaA_spawn(lua_State *L)
     g_strfreev(argv);
     if(!retval)
     {
-        /* push error on stack */
-        lua_pushstring(L, error->message);
-        warn("%s", lua_tostring(L, -1));
+        luaA_warn(L, "%s", error->message);
         g_error_free(error);
         if(context)
             sn_launcher_context_complete(context);

common/luaobject.c
@@ -254,7 +254,7 @@ signal_object_emit(lua_State *L, signal_array_t *arr, const char *name, int narg
             luaA_dofunction(L, nargs, 0);
         }
     } else
-        warn("Trying to emit unknown signal '%s'", name);
+        luaA_warn(L, "Trying to emit unknown signal '%s'", name);
 
     /* remove args */
     lua_pop(L, nargs);
@@ -273,11 +273,11 @@ luaA_object_emit_signal(lua_State *L, int oud,
     lua_class_t *lua_class = luaA_class_get(L, oud);
     lua_object_t *obj = luaA_toudata(L, oud, lua_class);
     if(!obj) {
-        warn("Trying to emit signal '%s' on non-object", name);
+        luaA_warn(L, "Trying to emit signal '%s' on non-object", name);
         return;
     }
     else if(lua_class->checker && !lua_class->checker(obj)) {
-        warn("Trying to emit signal '%s' on invalid object", name);
+        luaA_warn(L, "Trying to emit signal '%s' on invalid object", name);
         return;
     }
     signal_t *sigfound = signal_array_getbyid(&obj->signals,
@@ -304,8 +304,10 @@ luaA_object_emit_signal(lua_State *L, int oud,
             lua_remove(L, - nargs - nbfunc - 2 + i);
             luaA_dofunction(L, nargs + 1, 0);
         }
-    } else
-        warn("Trying to emit unknown signal '%s'", name);
+    } else {
+        luaA_warn(L, "Trying to emit unknown signal '%s'", name);
+        return;
+    }
 
     /* Then emit signal on the class */
     lua_pushvalue(L, oud);

objects/key.c
@@ -91,14 +91,14 @@ luaA_keystore(lua_State *L, int ud, const char *str, ssize_t len)
 
             if(!g_utf8_validate(str, -1, NULL))
             {
-                warn("failed to convert \"%s\" into keysym (invalid UTF-8 string)", str);
+                luaA_warn(L, "failed to convert \"%s\" into keysym (invalid UTF-8 string)", str);
                 return;
             }
 
             length = g_utf8_strlen(str, -1); /* This function counts combining characters. */
             if(length <= 0)
             {
-                warn("failed to convert \"%s\" into keysym (empty UTF-8 string)", str);
+                luaA_warn(L, "failed to convert \"%s\" into keysym (empty UTF-8 string)", str);
                 return;
             }
             else if(length > 1)
@@ -107,7 +107,7 @@ luaA_keystore(lua_State *L, int ud, const char *str, ssize_t len)
                 if(g_utf8_strlen(composed, -1) != 1)
                 {
                     p_delete(&composed);
-                    warn("failed to convert \"%s\" into keysym (failed to compose a single character)", str);
+                    luaA_warn(L, "failed to convert \"%s\" into keysym (failed to compose a single character)", str);
                     return;
                 }
                 unicode = g_utf8_get_char(composed);
@@ -118,7 +118,7 @@ luaA_keystore(lua_State *L, int ud, const char *str, ssize_t len)
 
             if(unicode == (gunichar)-1 || unicode == (gunichar)-2)
             {
-                warn("failed to convert \"%s\" into keysym (neither keysym nor single unicode)", str);
+                luaA_warn(L, "failed to convert \"%s\" into keysym (neither keysym nor single unicode)", str);
                 return;
             }
 
@@ -132,7 +132,7 @@ luaA_keystore(lua_State *L, int ud, const char *str, ssize_t len)
                 key->keysym = unicode | (1 << 24);
             else
             {
-                warn("failed to convert \"%s\" into keysym (unicode out of range): \"%u\"", str, unicode);
+                luaA_warn(L, "failed to convert \"%s\" into keysym (unicode out of range): \"%u\"", str, unicode);
                 return;
             }
         }

objects/window.c
@@ -295,7 +295,7 @@ luaA_window_set_type(lua_State *L, window_t *w)
         type = WINDOW_TYPE_NORMAL;
     else
     {
-        warn("Unknown window type '%s'", buf);
+        luaA_warn(L, "Unknown window type '%s'", buf);
         return 0;
     }
 

commit 461da9ea4b96a94fb16055dd0c75c4832f081b75

Fix error handling in awesome.spawn()
Commit 5e6a893 broke error handling in awesome.spawn(): Instead of
returning an error message, it would just return its last argument.

This commit fixes that, removes some not-so-helpful warnings, and adds
lots of tests for this code.

Fixes: https://github.com/awesomeWM/awesome/issues/1281
Signed-off-by: Uli Schlachter <psychon@znc.in>

spawn.c
@@ -414,11 +414,11 @@ luaA_spawn(lua_State *L)
     {
         g_strfreev(argv);
         if (error) {
-            luaA_warn(L, "spawn: parse error: %s", error->message);
+            lua_pushfstring(L, "spawn: parse error: %s", error->message);
             g_error_free(error);
         }
         else
-            luaA_warn(L, "spawn: There is nothing to execute");
+            lua_pushliteral(L, "spawn: There is nothing to execute");
         return 1;
     }
 
@@ -443,7 +443,7 @@ luaA_spawn(lua_State *L)
     g_strfreev(argv);
     if(!retval)
     {
-        luaA_warn(L, "%s", error->message);
+        lua_pushstring(L, error->message);
         g_error_free(error);
         if(context)
             sn_launcher_context_complete(context);

tests/test-spawn.lua
@@ -10,6 +10,31 @@ local exit_yay, exit_snd = nil, nil
 -- * spawn with startup notification is covered by test-spawn-snid.lua
 
 local steps = {
+  function()
+    -- Test various error conditions. There are quite a number of them...
+    local error_message
+
+    error_message = spawn("this_does_not_exist_and_should_fail")
+    assert(string.find(error_message, 'No such file or directory'), error_message)
+
+    error_message = spawn({"this_does_not_exist_and_should_fail"})
+    assert(string.find(error_message, 'No such file or directory'), error_message)
+
+    error_message = spawn("foo '")
+    assert(string.find(error_message, 'parse error: Text ended before matching quote was found'), error_message)
+
+    error_message = spawn()
+    assert(string.find(error_message, 'No command to execute'), error_message)
+
+    error_message = spawn(" ")
+    assert(string.find(error_message, 'Text was empty'), error_message)
+
+    error_message = spawn{}
+    assert(string.find(error_message, 'There is nothing to execute'), error_message)
+
+    return true
+  end,
+
   function(count)
     if count == 1 then
       local steps_yay = 0