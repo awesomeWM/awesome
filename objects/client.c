e5be281ff3883124cebe7a630af0e9bc595207fb
Uli Schlachter <psychon@znc.in>
2016-12-31 15:31:41 +0100

    client_set_sticky: Update screen workarea
    This partially fixes test-struts.lua. The test now fails at some later
    part.
    
    Signed-off-by: Uli Schlachter <psychon@znc.in>

--- objects/client.c
+++ objects/client.c
@@ -1797,6 +1797,8 @@ client_set_sticky(lua_State *L, int cidx, bool s)
     {
         c->sticky = s;
         banning_need_update();
+        if(strut_has_value(&c->strut))
+            screen_update_workarea(c->screen);
         luaA_object_emit_signal(L, cidx, "property::sticky", 0);
     }
 }
