767cda5830a5a0fc06165e793d9cc14fbbf50735
Emmanuel Lepage Vallee <elv1313@gmail.com>
2017-01-01 15:48:38 -0500

    doc: Fix some example tests indentation
    When `DOC_NO_USAGE` is set, the examples need 4 spaces as per the
    markdown spec.

--- tests/examples/wibox/widget/checkbox/custom.lua
+++ tests/examples/wibox/widget/checkbox/custom.lua
@@ -3,33 +3,31 @@ local wibox     = require( "wibox"     ) --DOC_HIDE
 local beautiful = require( "beautiful" ) --DOC_HIDE
 
 parent:add( --DOC_HIDE
-           wibox.widget {
-               checked       = true,
-               color         = beautiful.bg_normal,
-               paddings      = 2,
-               forced_width  = 20, --DOC_HIDE
-               forced_height = 20, --DOC_HIDE
-               check_shape   = function(cr, width, height)
-                   local rs = math.min(width, height)
-
-                   cr:move_to( 0  , 0  )
-                   cr:line_to( rs , 0  )
-                   cr:move_to( 0  , 0  )
-                   cr:line_to( 0  , rs )
-                   cr:move_to( 0  , rs )
-                   cr:line_to( rs , rs )
-                   cr:move_to( rs , 0  )
-                   cr:line_to( rs , rs )
-                   cr:move_to( 0  , 0  )
-                   cr:line_to( rs , rs )
-                   cr:move_to( 0  , rs )
-                   cr:line_to( rs , 0  )
-               end,
-               check_border_color = "#ff0000",
-               check_color        = "#00000000",
-               check_border_width = 1,
-               widget             = wibox.widget.checkbox
-           }
-           ) --DOC_HIDE
-
+    wibox.widget {
+        checked       = true,
+        color         = beautiful.bg_normal,
+        paddings      = 2,
+        forced_width  = 20, --DOC_HIDE
+        forced_height = 20, --DOC_HIDE
+        check_shape   = function(cr, width, height)
+            local rs = math.min(width, height)
+            cr:move_to( 0  , 0  )
+            cr:line_to( rs , 0  )
+            cr:move_to( 0  , 0  )
+            cr:line_to( 0  , rs )
+            cr:move_to( 0  , rs )
+            cr:line_to( rs , rs )
+            cr:move_to( rs , 0  )
+            cr:line_to( rs , rs )
+            cr:move_to( 0  , 0  )
+            cr:line_to( rs , rs )
+            cr:move_to( 0  , rs )
+            cr:line_to( rs , 0  )
+        end,
+        check_border_color = "#ff0000",
+        check_color        = "#00000000",
+        check_border_width = 1,
+        widget             = wibox.widget.checkbox
+    }
+) --DOC_HIDE
 --DOC_HIDE vim: filetype=lua:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:textwidth=80
