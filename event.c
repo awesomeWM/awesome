awesomeWM/awesome

commit baaff93a7349a9da67b4b8f291a62820828f14bd

Only configure client geometries once per main loop iteration
This should "protect" the user from some stupidities that Lua code might be
doing that e.g. makes a client jump to another position and then immediately
back to where it was before. Only the last change in a single main loop
iteration will actually have any effect.

Original idea by Daniel here: https://github.com/awesomeWM/awesome/pull/174

Signed-off-by: Uli Schlachter <psychon@znc.in>

objects/client.c
@@ -1135,9 +1135,55 @@ client_border_refresh(void)
         window_border_refresh((window_t *) *c);
 }
 
+static void
+client_geometry_refresh(void)
+{
+    client_ignore_enterleave_events();
+    foreach(_c, globalconf.clients)
+    {
+        client_t *c = *_c;
+
+        /* Compute the client window's and frame window's geometry */
+        area_t geometry = c->geometry;
+        area_t real_geometry = c->geometry;
+        if (!c->fullscreen)
+        {
+            real_geometry.x = c->titlebar[CLIENT_TITLEBAR_LEFT].size;
+            real_geometry.y = c->titlebar[CLIENT_TITLEBAR_TOP].size;
+            real_geometry.width -= c->titlebar[CLIENT_TITLEBAR_LEFT].size;
+            real_geometry.width -= c->titlebar[CLIENT_TITLEBAR_RIGHT].size;
+            real_geometry.height -= c->titlebar[CLIENT_TITLEBAR_TOP].size;
+            real_geometry.height -= c->titlebar[CLIENT_TITLEBAR_BOTTOM].size;
+        } else {
+            real_geometry.x = 0;
+            real_geometry.y = 0;
+        }
+
+        /* Is there anything to do? */
+        if (AREA_EQUAL(geometry, c->x11_frame_geometry)
+                && AREA_EQUAL(real_geometry, c->x11_client_geometry))
+            continue;
+
+        xcb_configure_window(globalconf.connection, c->frame_window,
+                XCB_CONFIG_WINDOW_X | XCB_CONFIG_WINDOW_Y | XCB_CONFIG_WINDOW_WIDTH | XCB_CONFIG_WINDOW_HEIGHT,
+                (uint32_t[]) { geometry.x, geometry.y, geometry.width, geometry.height });
+        xcb_configure_window(globalconf.connection, c->window,
+                XCB_CONFIG_WINDOW_X | XCB_CONFIG_WINDOW_Y | XCB_CONFIG_WINDOW_WIDTH | XCB_CONFIG_WINDOW_HEIGHT,
+                (uint32_t[]) { real_geometry.x, real_geometry.y, real_geometry.width, real_geometry.height });
+
+        c->x11_frame_geometry = geometry;
+        c->x11_client_geometry = real_geometry;
+
+        /* ICCCM 4.2.3 says something else, but Java always needs this... */
+        client_send_configure(c);
+    }
+    client_restore_enterleave_events();
+}
+
 void
 client_refresh(void)
 {
+    client_geometry_refresh();
     client_border_refresh();
     client_focus_refresh();
 }
@@ -1516,36 +1562,6 @@ client_resize_do(client_t *c, area_t geometry)
     /* Also store geometry including border */
     area_t old_geometry = c->geometry;
     c->geometry = geometry;
-
-    /* Ignore all spurious enter/leave notify events */
-    client_ignore_enterleave_events();
-
-    /* Configure the client for its new size */
-    area_t real_geometry = geometry;
-    if (!c->fullscreen)
-    {
-        real_geometry.x = c->titlebar[CLIENT_TITLEBAR_LEFT].size;
-        real_geometry.y = c->titlebar[CLIENT_TITLEBAR_TOP].size;
-        real_geometry.width -= c->titlebar[CLIENT_TITLEBAR_LEFT].size;
-        real_geometry.width -= c->titlebar[CLIENT_TITLEBAR_RIGHT].size;
-        real_geometry.height -= c->titlebar[CLIENT_TITLEBAR_TOP].size;
-        real_geometry.height -= c->titlebar[CLIENT_TITLEBAR_BOTTOM].size;
-    } else {
-        real_geometry.x = 0;
-        real_geometry.y = 0;
-    }
-
-    xcb_configure_window(globalconf.connection, c->frame_window,
-            XCB_CONFIG_WINDOW_X | XCB_CONFIG_WINDOW_Y | XCB_CONFIG_WINDOW_WIDTH | XCB_CONFIG_WINDOW_HEIGHT,
-            (uint32_t[]) { geometry.x, geometry.y, geometry.width, geometry.height });
-    xcb_configure_window(globalconf.connection, c->window,
-            XCB_CONFIG_WINDOW_X | XCB_CONFIG_WINDOW_Y | XCB_CONFIG_WINDOW_WIDTH | XCB_CONFIG_WINDOW_HEIGHT,
-            (uint32_t[]) { real_geometry.x, real_geometry.y, real_geometry.width, real_geometry.height });
-
-    /* ICCCM 4.2.3 says something else, but Java always needs this... */
-    client_send_configure(c);
-
-    client_restore_enterleave_events();
 
     luaA_object_push(L, c);
     if (!AREA_EQUAL(old_geometry, geometry))

objects/client.h
@@ -61,6 +61,9 @@ struct client_t
     char *class, *instance;
     /** Window geometry */
     area_t geometry;
+    /** Old window geometry currently configured in X11 */
+    area_t x11_client_geometry;
+    area_t x11_frame_geometry;
     /** Startup ID */
     char *startup_id;
     /** True if the client is sticky */

commit 3da4802adb8a79a604c5d75ae27b35c0cdad4174

Reply on configurerequest even if geometry is the same
Bug was introduced by baaff93a7349a.

Fixes: https://github.com/awesomeWM/awesome/issues/1340
Fixes: https://github.com/awesomeWM/awesome/issues/1369

event.c
@@ -395,9 +395,8 @@ event_handle_configurerequest(xcb_configure_request_event_t *ev)
                 geometry.y += diff_y;
         }
 
-        if(!client_resize(c, geometry, false))
-            /* ICCCM 4.1.5 / 4.2.3, if nothing was changed, send an event saying so */
-            client_send_configure(c);
+        c->got_configure_request = true;
+        client_resize(c, geometry, false);
     }
     else if (xembed_getbywin(&globalconf.embedded, ev->window))
     {

objects/client.c
@@ -1210,8 +1210,14 @@ client_geometry_refresh(void)
 
         /* Is there anything to do? */
         if (AREA_EQUAL(geometry, c->x11_frame_geometry)
-                && AREA_EQUAL(real_geometry, c->x11_client_geometry))
+                && AREA_EQUAL(real_geometry, c->x11_client_geometry)) {
+            if (c->got_configure_request) {
+                /* ICCCM 4.1.5 / 4.2.3, if nothing was changed, send an event saying so */
+                client_send_configure(c);
+                c->got_configure_request = false;
+            }
             continue;
+        }
 
         if (!ignored_enterleave) {
             client_ignore_enterleave_events();
@@ -1230,6 +1236,7 @@ client_geometry_refresh(void)
 
         /* ICCCM 4.2.3 says something else, but Java always needs this... */
         client_send_configure(c);
+        c->got_configure_request = false;
     }
     if (ignored_enterleave)
         client_restore_enterleave_events();

objects/client.h
@@ -64,6 +64,8 @@ struct client_t
     /** Old window geometry currently configured in X11 */
     area_t x11_client_geometry;
     area_t x11_frame_geometry;
+    /** Got a configure request and have to call client_send_configure() if its ignored? */
+    bool got_configure_request;
     /** Startup ID */
     char *startup_id;
     /** True if the client is sticky */