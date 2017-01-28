74508098de42c24b37fde7d4b50102a9ab72b656
Emmanuel Lepage Vallee <elv1313@gmail.com>
2017-01-28 16:12:39 -0500

    rules: Add a rule building chain of responsability
    Before this commit, there was a conflict between the spawn and
    awful.rules rules.
    
    Also, modules such as Tryannical monkey-patched this function to
    add their own rules to the mix. This commit introduce a proper
    API to add handlers.
    
    The order is crutial for this to work, so a dependency system is
    also added.
    
    Fix #1482

--- lib/awful/spawn.lua
+++ lib/awful/spawn.lua
@@ -165,6 +165,7 @@ local lgi = require("lgi")
 local Gio = lgi.Gio
 local GLib = lgi.GLib
 local util   = require("awful.util")
+local timer  = require("gears.timer")
 local protected_call = require("gears.protected_call")
 
 local spawn = {}
@@ -217,7 +218,10 @@ function spawn.on_snid_callback(c)
         local props = entry[1]
         local callback = entry[2]
         c:emit_signal("spawn::completed_with_payload", props, callback)
-        spawn.snid_buffer[c.startup_id] = nil
+
+        timer.delayed_call(function()
+            spawn.snid_buffer[c.startup_id] = nil
+        end)
     end
 end
 
