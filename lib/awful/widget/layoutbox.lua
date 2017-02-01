f6d4b9b0a5f5f8a76f66ba60d385630759c178e9
Uli Schlachter <psychon@znc.in>
2017-02-01 19:22:45 +0100

    layoutbox: Update when a tag's screen changes
    In theory it would be enough to only update the old and new screen of
    the tag whose screen changed. However, we cannot easily get the old
    screen, so just update all layoutboxes.
    
    Fixes: https://github.com/awesomeWM/awesome/issues/1503
    Signed-off-by: Uli Schlachter <psychon@znc.in>

--- lib/awful/widget/layoutbox.lua
+++ lib/awful/widget/layoutbox.lua
@@ -48,6 +48,13 @@ function layoutbox.new(screen)
         boxes = setmetatable({}, { __mode = "kv" })
         capi.tag.connect_signal("property::selected", update_from_tag)
         capi.tag.connect_signal("property::layout", update_from_tag)
+        capi.tag.connect_signal("property::screen", function()
+            for s, w in pairs(boxes) do
+                if s.valid then
+                    update(w, s)
+                end
+            end
+        end)
         layoutbox.boxes = boxes
     end
 
