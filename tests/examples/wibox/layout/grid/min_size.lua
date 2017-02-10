bf2d658380890f9b85cc58c56371d8984ec427dd
getzze <getzze@gmail.com>
2017-02-10 00:10:12 +0000

    Add tests for grid layout
    Update tests
    
    Update tests
    
    Separate spacing and padding tests
    
    Update tests amd add imperative usage
    
    Change fixed_size to homogeneous property
    
    Update tests

---
+++ tests/examples/wibox/layout/grid/min_size.lua
@@ -0,0 +1,49 @@
+local generic_widget = ... --DOC_HIDE_ALL
+local wibox     = require("wibox") --DOC_HIDE
+local beautiful = require("beautiful") --DOC_HIDE
+
+local w = wibox.widget {
+    {
+        {
+            markup = "<b>min_cols_size</b> = <i>0</i>",
+            widget = wibox.widget.textbox
+        },
+        {
+            {
+                generic_widget( "first"  ),
+                generic_widget( "second" ),
+                forced_num_cols = 2,
+                min_cols_size   = 0,
+                homogeneous     = true,
+                layout          = wibox.layout.grid,
+            },
+            margins = 1,
+            color  = beautiful.border_color,
+            layout = wibox.container.margin,
+        },
+        layout = wibox.layout.fixed.vertical
+    },
+    {
+        {
+            markup = "<b>min_cols_size</b> = <i>100</i>",
+            widget = wibox.widget.textbox
+        },
+        {
+            {
+                generic_widget( "first"  ),
+                generic_widget( "second" ),
+                forced_num_cols = 2,
+                min_cols_size   = 100,
+                homogeneous     = true,
+                layout          = wibox.layout.grid,
+            },
+            margins = 1,
+            color  = beautiful.border_color,
+            layout = wibox.container.margin,
+        },
+        layout = wibox.layout.fixed.vertical
+    },
+    layout = wibox.layout.fixed.horizontal
+}
+return w, w:fit({dpi=96}, 9999, 9999)
+--return w, 500, 50
