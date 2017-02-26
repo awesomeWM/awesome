6616ba20c7b999e02e916e316b2c778e958b02a6
BuildTools <wacossusca34@gmail.com>
2017-02-25 22:47:15 -0800

    emit ewmh geometry requests as signals

--- lib/awful/ewmh.lua
+++ lib/awful/ewmh.lua
@@ -246,6 +246,12 @@ local context_mapper = {
 function ewmh.geometry(c, context, hints)
     local layout = c.screen.selected_tag and c.screen.selected_tag.layout or nil
 
+    -- For dockable clients, always apply new geometry
+    if c.dockable and context == "ewmh" then
+        c:geometry(hints.geometry)
+        return
+    end
+
     -- Setting the geometry will not work unless the client is floating.
     if (not c.floating) and (not layout == asuit.floating) then
         return
