20501fb2f7a4f3557074e74182e778f5cf4f4866
Christoph Mertz <chris@nimel.de>
2017-02-27 22:32:39 +0100

    gears.geometry: replace fallback with ^
    This should work with all supported lua versions
    (i.e. from 5.1 upwards), not sure about luajit though.
    
    Signed-off-by: Christoph Mertz <chris@nimel.de>

--- lib/gears/geometry.lua
+++ lib/gears/geometry.lua
@@ -11,10 +11,6 @@
 ---------------------------------------------------------------------------
 local math = math
 
-local pow = math.pow or function(a,b) -- lua 5.3 compat
-    return a^b
-end
-
 local gears = {geometry = {rectangle = {} } }
 
 --- Get the square distance between a rectangle and a point.
@@ -121,7 +117,7 @@ local function calculate_distance(dir, _gA, _gB)
         gAx = _gA.x + _gA.width
     end
 
-    return math.sqrt(pow(gBx - gAx, 2) + pow(gBy - gAy, 2))
+    return math.sqrt((gBx - gAx) ^ 2 + (gBy - gAy) ^ 2)
 end
 
 --- Get the nearest rectangle in the given direction. Every rectangle is specified as a table
