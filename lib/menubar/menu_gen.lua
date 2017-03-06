862bfbf795b619dda9079ddfd20a2e6b1864a352
Jan Larres <jan@majutsushi.net>
2017-03-07 10:58:09 +1300

    menubar: add trailing slash with $XDG_DATA_HOME (#1631)
    The $XDG_DATA_HOME environment variable may not necessarily end in a
    slash, so insert it when generating the list of all menu dirs to make
    sure the directory can be found.

--- lib/menubar/menu_gen.lua
+++ lib/menubar/menu_gen.lua
@@ -20,12 +20,12 @@ local menu_gen = {}
 
 local data_dir = os.getenv("XDG_DATA_HOME")
 if not data_dir then
-    data_dir = os.getenv("HOME") .. '/.local/share/'
+    data_dir = os.getenv("HOME") .. '/.local/share'
 end
 
 --- Specifies all directories where menubar should look for .desktop
 -- files. The search is recursive.
-menu_gen.all_menu_dirs = { data_dir .. 'applications/', '/usr/share/applications/', '/usr/local/share/applications/' }
+menu_gen.all_menu_dirs = { data_dir .. '/applications/', '/usr/share/applications/', '/usr/local/share/applications/' }
 
 --- Specify the mapping of .desktop Categories section to the
 -- categories in the menubar. If "use" flag is set to false then any of
