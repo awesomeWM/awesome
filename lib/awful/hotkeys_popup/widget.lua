awesomeWM/awesome

commit c183588ba1b9ad0e77cbe460c999aeb19e4b307a

Allow to add group rules to hotkeys_popup default instance

lib/awful/hotkeys_popup/widget.lua
@@ -534,7 +534,6 @@ function widget.new(args)
         end)
     end
 
-
     --- Add hotkey descriptions for third-party applications.
     -- @tparam table hotkeys Table with bindings,
     -- see `awful.hotkeys_popup.key.vim` as an example.
@@ -556,6 +555,13 @@ function widget.new(args)
         self:_sort_hotkeys(self._additional_hotkeys)
     end
 
+    --- Add hotkey group rules for third-party applications.
+    -- @tparam group hotkeys group name,
+    -- @tparam data rule data for the group
+    -- see `awful.hotkeys_popup.key.vim` as an example.
+    function widget_instance:add_group_rules(group, data)
+        self.group_rules[group] = data
+    end
 
     return widget_instance
 end
@@ -580,6 +586,15 @@ end
 -- see `awful.hotkeys_popup.key.vim` as an example.
 function widget.add_hotkeys(...)
     return get_default_widget():add_hotkeys(...)
+end
+
+--- Add hotkey group rules for third-party applications
+-- (default widget instance will be used).
+-- @tparam string hotkeys group name,
+-- @tparam table rule data for the group
+-- see `awful.hotkeys_popup.key.vim` as an example.
+function widget.add_group_rules(...)
+    return get_default_widget():add_group_rules(...)
 end
 
 return widget

lib/awful/hotkeys_popup/keys/firefox.lua
@@ -1,0 +1,33 @@
+local hotkeys_popup = require("awful.hotkeys_popup.widget")
+local fire_rule = { class = { "Firefox" } }
+for group_name, group_data in pairs({
+    ["Firefox: tabs"] = { color = "#009F00", rule_any = fire_rule }
+}) do
+    hotkeys_popup.add_group_rules(group_name, group_data)
+end
+
+local firefox_keys = {
+
+    ["Firefox: tabs"] = {{
+        modifiers = { "Mod1" },
+        keys = {
+            ["1..9"] = "go to tab"
+        }
+    }, {
+        modifiers = { "Ctrl" },
+        keys = {
+            t = "new tab",
+            w = 'close tab',
+            ['Tab'] = "next tab"
+        }
+    }, {
+        modifiers = { "Ctrl", "Shift" },
+        keys = {
+          ['Tab'] = "previous tab"
+        }
+    }}
+}
+
+hotkeys_popup.add_hotkeys(firefox_keys)
+
+-- vim: filetype=lua:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:textwidth=80

lib/awful/hotkeys_popup/keys/vim.lua
@@ -17,7 +17,7 @@ for group_name, group_data in pairs({
     ["VIM: find"] =               { color="#65cF9F", rule_any=vim_rule_any },
     ["VIM: scroll"] =             { color="#659FdF", rule_any=vim_rule_any },
 }) do
-    hotkeys_popup.group_rules[group_name] = group_data
+    hotkeys_popup.add_group_rules(group_name, group_data)
 end
 
 

commit f8678d39a4586906b0455250a5eb689556d9e39d

Fix parameters docs in awful.hotkeys_popup.widget
This fixes broken API docs introduced in commit c183588ba1b9ad0e77c.

Signed-off-by: Uli Schlachter <psychon@znc.in>

lib/awful/hotkeys_popup/widget.lua
@@ -556,8 +556,8 @@ function widget.new(args)
     end
 
     --- Add hotkey group rules for third-party applications.
-    -- @tparam group hotkeys group name,
-    -- @tparam data rule data for the group
+    -- @tparam string group hotkeys group name,
+    -- @tparam table data rule data for the group
     -- see `awful.hotkeys_popup.key.vim` as an example.
     function widget_instance:add_group_rules(group, data)
         self.group_rules[group] = data
@@ -590,11 +590,11 @@ end
 
 --- Add hotkey group rules for third-party applications
 -- (default widget instance will be used).
--- @tparam string hotkeys group name,
--- @tparam table rule data for the group
+-- @tparam string group rule group name,
+-- @tparam table data rule data for the group
 -- see `awful.hotkeys_popup.key.vim` as an example.
-function widget.add_group_rules(...)
-    return get_default_widget():add_group_rules(...)
+function widget.add_group_rules(group, data)
+    return get_default_widget():add_group_rules(group, data)
 end
 
 return widget