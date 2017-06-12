82d1616773adf3172495b91aa9408989da3fde87
Emmanuel Lepage Vallee <elv1313@gmail.com>
2017-06-12 02:48:34 -0400

    tests: Add tests for the stack layout

---
+++ tests/examples/wibox/layout/stack/spacing.lua
@@ -0,0 +1,13 @@
+local generic_widget = ... --DOC_HIDE
+local wibox     = require("wibox") --DOC_HIDE
+
+return --DOC_HIDE
+wibox.widget {
+    generic_widget( "first"  ),
+    generic_widget( "second" ),
+    generic_widget( "third"  ),
+    spacing = 6,
+    layout  = wibox.layout.stack
+}
+
+--DOC_HIDE vim: filetype=lua:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:textwidth=80
