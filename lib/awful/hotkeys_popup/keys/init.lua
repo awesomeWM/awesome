c5e830cd6031643eb8046c9f05145ad7a54f58ce
Yauhen Kirylau <actionless.loveless@gmail.com>
2017-06-13 02:39:46 +0200

    fix(awful: hotkeys_popup: keys): require all modules in init.lua

--- lib/awful/hotkeys_popup/keys/init.lua
+++ lib/awful/hotkeys_popup/keys/init.lua
@@ -8,7 +8,10 @@
 
 
 local keys = {
-  vim = require("awful.hotkeys_popup.keys.vim")
+  vim = require("awful.hotkeys_popup.keys.vim"),
+  firefox = require("awful.hotkeys_popup.keys.firefox"),
+  tmux = require("awful.hotkeys_popup.keys.tmux"),
+  qutebrowser = require("awful.hotkeys_popup.keys.qutebrowser"),
 }
 return keys
 
