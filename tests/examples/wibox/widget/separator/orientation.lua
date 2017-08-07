d37d13395ef465c0a48c32e8e9cb95dfffc01a1d
Emmanuel Lepage Vallee <elv1313@gmail.com>
2017-08-07 16:12:12 -0400

    tests: Test the separator widget

---
+++ tests/examples/wibox/widget/separator/orientation.lua
@@ -0,0 +1,36 @@
+local parent = ... --DOC_HIDE_ALL
+local wibox  = require("wibox")
+
+parent:add(
+wibox.widget {
+    {
+        {
+            markup = "<b>orientation</b> = <i>horizontal</i>",
+            widget = wibox.widget.textbox
+        },
+        {
+            orientation = "horizontal",
+            widget = wibox.widget.separator,
+        },
+        forced_width = 200,
+        layout = wibox.layout.fixed.vertical
+    },
+    {
+        {
+            markup = "<b>orientation</b> = <i>vertical</i>",
+            widget = wibox.widget.textbox
+        },
+        {
+            orientation = "vertical",
+            widget = wibox.widget.separator,
+        },
+        forced_width = 200,
+        layout = wibox.layout.fixed.vertical
+    },
+    forced_height = 30,
+    forced_width = 400,
+    layout = wibox.layout.fixed.horizontal
+}
+)
+
+--DOC_HIDE vim: filetype=lua:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:textwidth=80
