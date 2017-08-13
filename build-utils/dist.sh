30ac1bb8bf9700247c904c873df1b9abe08dfb34
Daniel Hahler <git@thequod.de>
2017-08-13 12:14:57 +0200

    build-utils/dist.sh: fix shellcheck warnings and add usage

--- build-utils/dist.sh
+++ build-utils/dist.sh
@@ -4,20 +4,23 @@
 
 set -e
 
+if [ -z "$1" ]; then
+    echo "Usage: $0 VERSION"
+    exit 64
+fi
+
 VERSION=$1
-SVERSION=`echo $1 | sed 's/^v//'`
-git archive --prefix=dist/awesome-$SVERSION/ $VERSION | tar -xf -
+SVERSION=$(echo "$VERSION" | sed 's/^v//')
+git archive --prefix "dist/awesome-$SVERSION/" "$VERSION" | tar -xf -
 cd dist
-echo -n $VERSION > awesome-$SVERSION/.version_stamp
-tar cjf awesome-$SVERSION.tar.bz2 awesome-$SVERSION
-tar cJf awesome-$SVERSION.tar.xz awesome-$SVERSION
-gpg --armor --detach-sign awesome-$SVERSION.tar.bz2
-gpg --armor --detach-sign awesome-$SVERSION.tar.xz
+printf '%s' "$VERSION" > "awesome-$SVERSION/.version_stamp"
+tar cjf "awesome-$SVERSION.tar.bz2" "awesome-$SVERSION"
+tar cJf "awesome-$SVERSION.tar.xz" "awesome-$SVERSION"
+gpg --armor --detach-sign "awesome-$SVERSION.tar.bz2"
+gpg --armor --detach-sign "awesome-$SVERSION.tar.xz"
 
 echo "Created the following files in dist/:"
-echo "awesome-$SVERSION.tar.bz2"
-echo "awesome-$SVERSION.tar.xz"
-echo "awesome-$SVERSION.tar.bz2.asc"
-echo "awesome-$SVERSION.tar.xz.asc"
+ls -l "awesome-$SVERSION.tar.bz2"     "awesome-$SVERSION.tar.xz" \
+      "awesome-$SVERSION.tar.bz2.asc" "awesome-$SVERSION.tar.xz.asc"
 
 # vim: filetype=sh:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:textwidth=80
