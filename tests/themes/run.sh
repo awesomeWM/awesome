ee9e2a7e2735b3d81d388d1a8ca697fc16489e29
Daniel Hahler <git@thequod.de>
2017-08-16 00:14:57 +0200

    fixup! tests/themes/run.sh: use themes from source dir

--- tests/themes/run.sh
+++ tests/themes/run.sh
@@ -6,14 +6,13 @@
 
 set -e
 
-if ! [ -f CMakeCache.txt ]; then
-  echo 'This should be run from the build directory (expected CMakeCache.txt).' >&2
+if ! [ -f CMakeLists.txt ]; then
+  echo 'This should be run from the source directory (expected CMakeLists.txt).' >&2
   exit 64
 fi
-build_dir="$PWD"
-src_dir="$(dirname -- "$0")/../.."
+source_dir="$PWD"
 
-config_file="$build_dir/test-themes-awesomerc.lua"
+config_file="$(mktemp)"
 
 for theme_file in themes/*/theme.lua; do
   echo "==== Testing theme: $theme_file ===="
@@ -22,10 +21,8 @@ for theme_file in themes/*/theme.lua; do
 
   sed "s~default/theme~$theme_name/theme~g" "awesomerc.lua" > "$config_file"
 
-  # Set CMAKE_BINARY_DIR for out-of-tree builds.
-  CMAKE_BINARY_DIR="$PWD" \
   AWESOME_RC_FILE="$config_file" \
-    AWESOME_THEMES_PATH="$src_dir/themes" \
+    AWESOME_THEMES_PATH="$source_dir/themes" \
     AWESOME_ICON_PATH="$PWD/icons" \
-    "$src_dir/tests/run.sh" themes/tests.lua
+    "$source_dir/tests/run.sh" themes/tests.lua
 done
