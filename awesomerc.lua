5fc5eecef16886d15723e0f2f4cdfba574804f70
Uli Schlachter <psychon@znc.in>
2017-08-16 11:34:47 +0200

    Automatically make LuaRocks modules available in awesome
    Running eval $(luarocks path --bin) before running awesome is not all
    that easy. It's a lot easier to do the equivalent for this in rc.lua.
    Plus, if LuaRocks is not installed, this will silently do nothing.
    
    Signed-off-by: Uli Schlachter <psychon@znc.in>

--- awesomerc.lua
+++ awesomerc.lua
@@ -1,3 +1,7 @@
+-- If LuaRocks is installed, make sure that packages installed through it are
+-- found (e.g. lgi). If LuaRocks is not installed, do nothing.
+pcall(require, "luarocks.loader")
+
 -- @DOC_REQUIRE_SECTION@
 -- Standard awesome library
 local gears = require("gears")
