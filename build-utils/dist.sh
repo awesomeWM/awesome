168644118f6c11a06f3e45be0a132b287fd8ef79
Daniel Hahler <git@thequod.de>
2017-08-30 21:06:16 +0200

    squash! build-utils/dist.sh: fix shellcheck warnings and add usage
    Touch version stamp and dir for reproducible builds
    
    [ci skip]

--- build-utils/dist.sh
+++ build-utils/dist.sh
@@ -5,17 +5,26 @@
 set -e
 
 if [ -z "$1" ]; then
-    echo "Usage: $0 VERSION"
+    echo "Usage: $0 <GIT_TAG>"
     exit 64
 fi
 
-VERSION=$1
-SVERSION=$(echo "$VERSION" | sed 's/^v//')
-git archive --prefix "dist/awesome-$SVERSION/" "$VERSION" | tar -xf -
+GIT_TAG="$1"
+SVERSION=$(echo "$GIT_TAG" | sed 's/^v//')
+
+date=$(git log -1 --format=%cI "$GIT_TAG")
+git archive --prefix "dist/awesome-$SVERSION/" "$GIT_TAG" | tar -xf -
+
 cd dist
-printf '%s' "$VERSION" > "awesome-$SVERSION/.version_stamp"
-tar cjf "awesome-$SVERSION.tar.bz2" "awesome-$SVERSION"
-tar cJf "awesome-$SVERSION.tar.xz" "awesome-$SVERSION"
+version_stamp="awesome-$SVERSION/.version_stamp"
+printf '%s' "$GIT_TAG" > "$version_stamp"
+touch --date="$date" "$version_stamp" "awesome-$SVERSION"
+
+tar cf "awesome-$SVERSION.tar" "awesome-$SVERSION"
+bzip2 -c "awesome-$SVERSION.tar" > "awesome-$SVERSION.tar.bz2"
+xz -c "awesome-$SVERSION.tar" > "awesome-$SVERSION.tar.xz"
+rm "awesome-$SVERSION.tar"
+
 gpg --armor --detach-sign "awesome-$SVERSION.tar.bz2"
 gpg --armor --detach-sign "awesome-$SVERSION.tar.xz"
 
