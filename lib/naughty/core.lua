awesomeWM/awesome

commit be29ee676882b98601ab5276f21cc50c191ebfe4

naughty: avoid notification flickering on replace (#1785)
Fixes https://github.com/awesomeWM/awesome/issues/1737.

lib/naughty/core.lua
@@ -334,8 +334,9 @@ end
 --
 -- @param notification Notification object to be destroyed
 -- @param reason One of the reasons from notificationClosedReason
+-- @param[opt=false] keep_visible If true, keep the notification visible
 -- @return True if the popup was successfully destroyed, nil otherwise
-function naughty.destroy(notification, reason)
+function naughty.destroy(notification, reason, keep_visible)
     if notification and notification.box.visible then
         if suspended then
             for k, v in pairs(naughty.notifications.suspended) do
@@ -350,8 +351,12 @@ function naughty.destroy(notification, reason)
         if notification.timer then
             notification.timer:stop()
         end
-        notification.box.visible = false
-        arrange(scr)
+
+        if not keep_visible then
+            notification.box.visible = false
+            arrange(scr)
+        end
+
         if notification.destroy_cb and reason ~= naughty.notificationClosedReason.silent then
             notification.destroy_cb(reason or naughty.notificationClosedReason.undefined)
         end
@@ -610,11 +615,13 @@ function naughty.notify(args)
     local notification = { screen = s, destroy_cb = destroy_cb, timeout = timeout }
 
     -- replace notification if needed
+    local reuse_box
     if args.replaces_id then
         local obj = naughty.getById(args.replaces_id)
         if obj then
             -- destroy this and ...
-            naughty.destroy(obj, naughty.notificationClosedReason.silent)
+            naughty.destroy(obj, naughty.notificationClosedReason.silent, true)
+            reuse_box = obj.box
         end
         -- ... may use its ID
         if args.replaces_id <= counter then
@@ -746,6 +753,10 @@ function naughty.notify(args)
                                shape_border_width = shape and border_width,
                                shape = shape,
                                type = "notification" })
+
+    if reuse_box then
+        notification.box = reuse_box
+    end
 
     if hover_timeout then notification.box:connect_signal("mouse::enter", hover_destroy) end
 

commit d9efb19ac40361888a2142d34e224f54fb7271b3

naughty: Fix replaces_id + changing colors
This fixes a regression introduced in be29ee67688. This commit changed
naughty to reuse an already-existing wibox when replaces_id is used,
instead of creating a new wibox. However, some of the properties that
are set only when creating a wibox were ignored due to this.

Fixes: https://github.com/awesomeWM/awesome/issues/2040
Signed-off-by: Uli Schlachter <psychon@znc.in>

lib/naughty/core.lua
@@ -775,18 +775,18 @@ function naughty.notify(args)
     notification.iconbox = iconbox
 
     -- create container wibox
-    notification.box = wibox({ fg = fg,
-                               bg = bg,
-                               border_color = border_color,
-                               border_width = border_width,
-                               shape_border_color = shape and border_color,
-                               shape_border_width = shape and border_width,
-                               shape = shape,
-                               type = "notification" })
-
-    if reuse_box then
+    if not reuse_box then
+        notification.box = wibox({ type = "notification" })
+    else
         notification.box = reuse_box
     end
+    notification.box.fg = fg
+    notification.box.bg = bg
+    notification.box.border_color = border_color
+    notification.box.border_width = border_width
+    notification.box.shape_border_color = shape and border_color
+    notification.box.shape_border_width = shape and border_width
+    notification.box.shape = shape
 
     if hover_timeout then notification.box:connect_signal("mouse::enter", hover_destroy) end
 