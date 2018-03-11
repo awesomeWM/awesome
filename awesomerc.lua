2dcf331a3e42eef11c3b23db004f490019de27b9
Uli Schlachter <psychon@znc.in>
2018-03-11 15:38:00 +0100

    Default config: Align up a series of conditions
    Signed-off-by: Uli Schlachter <psychon@znc.in>

--- awesomerc.lua
+++ awesomerc.lua
@@ -533,8 +533,8 @@ client.connect_signal("manage", function (c)
     -- i.e. put it at the end of others instead of setting it master.
     -- if not awesome.startup then awful.client.setslave(c) end
 
-    if awesome.startup and
-      not c.size_hints.user_position
+    if awesome.startup
+      and not c.size_hints.user_position
       and not c.size_hints.program_position then
         -- Prevent clients from being unreachable after screen count changes.
         awful.placement.no_offscreen(c)
