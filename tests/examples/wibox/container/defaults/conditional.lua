24e2f908e71cf6da69c20d99b0f23a0027c3781a
Emmanuel Lepage Vallee <elv1313@gmail.com>
2018-04-16 15:31:53 -0400

    tests: Test the conditional container.

---
+++ tests/examples/wibox/container/defaults/conditional.lua
@@ -0,0 +1,26 @@
+--DOC_HIDE_ALL
+local wibox     = require("wibox")
+local gears     = {shape = require("gears.shape")}
+local beautiful = require("beautiful")
+
+-- There is literally nothing to show, but the example must exist for the
+-- indexing.
+
+return {
+    text   = "Before",
+    align  = "center",
+    valign = "center",
+    widget = wibox.widget.textbox,
+},
+{
+    {
+        text   = "After",
+        align  = "center",
+        valign = "center",
+        when   = function() return true end,
+        widget = wibox.widget.textbox,
+    },
+    widget             = wibox.container.conditional
+}
+
+--DOC_HIDE vim: filetype=lua:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:textwidth=80
