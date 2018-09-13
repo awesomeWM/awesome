awesomeWM/awesome

commit ebcc19844e25705270243c614a35ca138a68f152

fix(awful: placement: no_overlap): use new placement infrastructure (#2032)
Fixes #2030

lib/awful/placement.lua
@@ -863,10 +863,12 @@ end
 --- Place the client where there's place available with minimum overlap.
 --@DOC_awful_placement_no_overlap_EXAMPLE@
 -- @param c The client.
+-- @tparam[opt={}] table args Other arguments
 -- @treturn table The new geometry
-function placement.no_overlap(c)
+function placement.no_overlap(c, args)
     c = c or capi.client.focus
-    local geometry = area_common(c)
+    args = add_context(args, "no_overlap")
+    local geometry = geometry_common(c, args)
     local screen   = get_screen(c.screen or a_screen.getbycoord(geometry.x, geometry.y))
     local cls = client.visible(screen)
     local curlay = layout.get()
@@ -917,7 +919,8 @@ function placement.no_overlap(c)
     new.width = geometry.width
     new.height = geometry.height
 
-    return c:geometry({ x = new.x, y = new.y })
+    geometry_common(c, args, new)
+    return fix_new_geometry(new, args, true)
 end
 
 --- Place the client under the mouse.

commit db11c7e9dabc8edb4f59fdac25978883afad3be9

awful.placement: Fix client size corruption in no_overlap
The awful.placement.no_overlap function was adding the window border
width to the client width and height (this is performed in
area_common(), which is called by geometry_common()), but did not
reverse this operation by calling remove_border() before returning the
final geometry; because of this, using no_overlap resulted in increasing
the window width and height by 2*border_width.

The bug was probably introduced in commit ebcc19844e25705270 (before
that commit no_overlap changed the window position directly instead of
relying on the new placement infrastructure), but was not noticed
because of other problems (e.g., in the default configuration the result
of no_overlap was overridden by the buggy no_offscreen).

Signed-off-by: Sergey Vlasov <sigprof@gmail.com>

lib/awful/placement.lua
@@ -941,6 +941,7 @@ function placement.no_overlap(c, args)
     new.width = geometry.width
     new.height = geometry.height
 
+    remove_border(c, args, new)
     geometry_common(c, args, new)
     return fix_new_geometry(new, args, true)
 end