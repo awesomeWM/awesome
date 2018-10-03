03b8a35c5cdeb8899383a97d7589da4469712235
Daniel Hahler <git@thequod.de>
2018-10-03 17:37:35 +0200

    themes/xresources/assets.lua: use raw=true with deprecation

--- themes/xresources/assets.lua
+++ themes/xresources/assets.lua
@@ -1,2 +1,2 @@
-require("gears.debug").deprecate("Use beautiful.theme_assets instead.")
+require("gears.debug").deprecate("Use beautiful.theme_assets instead.", {raw=true})
 return require("beautiful.theme_assets")
