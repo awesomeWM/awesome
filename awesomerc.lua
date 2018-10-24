6392b1e5116460522293d2275c241c70b6aebd78
legion151 <42674335+legion151@users.noreply.github.com>
2018-10-24 11:20:43 +0200

    Update awesomerc.lua

--- awesomerc.lua
+++ awesomerc.lua
@@ -507,7 +507,9 @@ awful.rules.rules = {
           "Wpa_gui",
           "veromix",
           "xtightvncviewer"},
-
+            
+        -- Note that the name property shown in xprop might be set slightly after creation of the client
+        -- and the name shown there might not match defined rules here.
         name = {
           "Event Tester",  -- xev.
         },
