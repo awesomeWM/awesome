4c45d292dd8f467166e4ca0ca94335e507fa1ce1
ArenaL5 <37388405+ArenaL5@users.noreply.github.com>
2018-12-23 17:39:12 +0000

    Making Tor Browser a floating window by default.
    Responding to issue #2504 at awesomeWM/awesome

--- awesomerc.lua
+++ awesomerc.lua
@@ -506,7 +506,8 @@ awful.rules.rules = {
           "Sxiv",
           "Wpa_gui",
           "veromix",
-          "xtightvncviewer"},
+          "xtightvncviewer",
+          "Tor Browser"},
 
         -- Note that the name property shown in xprop might be set slightly after creation of the client
         -- and the name shown there might not match defined rules here.
