5268a89262935377b584be4d139ab5e9dfb096f9
ArenaL5 <37388405+ArenaL5@users.noreply.github.com>
2018-12-27 15:53:46 +0000

    Add files via upload
    Sorting list of clients and adding comment to Tor Browser's entry, [as requested.](https://github.com/awesomeWM/awesome/pull/2505)

--- awesomerc.lua
+++ awesomerc.lua
@@ -504,10 +504,10 @@ awful.rules.rules = {
           "Kruler",
           "MessageWin",  -- kalarm.
           "Sxiv",
+          "Tor Browser", -- Needs a fixed window size to avoid fingerprinting by screen size.
           "Wpa_gui",
           "veromix",
-          "xtightvncviewer",
-          "Tor Browser"},
+          "xtightvncviewer"},
 
         -- Note that the name property shown in xprop might be set slightly after creation of the client
         -- and the name shown there might not match defined rules here.
