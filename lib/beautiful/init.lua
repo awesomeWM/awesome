784b77d373745279de3a29acb0732b77e8977235
Sorky <43520033+Sorky@users.noreply.github.com>
2019-01-15 06:27:10 +1100

    Prototype: beautiful.init: Update init.lua #5 (#13)
    * Prototype: beautiful.init: Update init.lua #5
    
    Some behaviour that doesn't really seem right...
    
    Doesn't fail on beautiful.init( my_config ) when my_config = a function
    Doesn't fail on beautiful.init( my_config ) when my_config = a number
    
    As far as I can tell `if theme then` only catches the case where the protected_call fails
    
    Doesn't fail on beautiful.init( my_config ) when my_config = {}
    
    Would also be nice if it returned true if there was no problem so as to do...
    ```
    if not beautiful.init( path_this .. "themes/shelby/theme.lua" ) then
    	beautiful.init( path_this .. "themes/shelby/.last.theme.lua" ) -- a proven config 
    end
    ```
    The above would be nice assuming a theme error would not cause a full reversion to the default rc.lua
    
    * Update init.lua
    
    Extreme error messages
    
    * Update init.lua

--- lib/beautiful/init.lua
+++ lib/beautiful/init.lua
@@ -186,22 +186,27 @@ end
 --   containing all the theme values.
 function beautiful.init(config)
     if config then
+        local state, t_config, t_theme = nil, nil, nil
         local homedir = os.getenv("HOME")
 
         -- If `config` is the path to a theme file, run this file,
         -- otherwise if it is a theme table, save it.
-        if type(config) == 'string' then
+        t_config = type(config)
+        if t_config == 'string' then
             -- Expand the '~' $HOME shortcut
             config = config:gsub("^~/", homedir .. "/")
             local dir = Gio.File.new_for_path(config):get_parent()
             rawset(beautiful, "theme_path", dir and (dir:get_path().."/") or nil)
             theme = protected_call(dofile, config)
-        elseif type(config) == 'table' then
+            t_theme = type(theme)
+            state = t_theme == 'table' and next(theme)
+        elseif t_config == 'table' then
             rawset(beautiful, "theme_path", nil)
             theme = config
+            state = next(theme)
         end
 
-        if theme then
+        if state then
             -- expand '~'
             if homedir then
                 for k, v in pairs(theme) do
@@ -210,9 +215,16 @@ function beautiful.init(config)
             end
 
             if theme.font then set_font(theme.font) end
+            return true
         else
+            rawset(beautiful, "theme_path", nil)
             theme = {}
-            return gears_debug.print_error("beautiful: error loading theme file " .. config)
+            local file = t_config == 'string' and (" from: " .. config)
+            local err = (file and t_theme == 'table' and "got an empty table" .. file)
+                     or (file and t_theme ~= 'table' and "got a " .. t_theme .. file)
+                     or (t_config == 'table' and "got an empty table")
+                     or ("got a " .. t_config)
+            return gears_debug.print_error("beautiful: error loading theme: " .. err)
         end
     else
         return gears_debug.print_error("beautiful: error loading theme: no theme specified")
