9d1504794153023b8f3ee9aed9f341d1cf211d60
Sorky <43520033+Sorky@users.noreply.github.com>
2019-01-14 11:44:28 +1100

    Update awesomerc.lua

--- awesomerc.lua
+++ awesomerc.lua
@@ -112,7 +112,7 @@ myawesomemenu = {
    { "manual", terminal .. " -e man awesome" },
    { "edit config", editor_cmd .. " " .. awesome.conffile },
    { "restart", awesome.restart },
-   { "quit", function() awesome.quit() end}
+   { "quit", function() awesome.quit() end }
 }
 
 mymainmenu = awful.menu({ items = { { "awesome", myawesomemenu, beautiful.awesome_icon },
