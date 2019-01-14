awesomeWM/awesome

commit ca1247358464a0591aceced464de5d69526a5240

Make beautiful.foo = bar work properly
Without this, users would modify the beautiful table directly instead of
the theme. This made a difference for code using beautiful.get() to get
the theme.

Reference: https://github.com/awesomeWM/awesome/pull/1854
Signed-off-by: Uli Schlachter <psychon@znc.in>

lib/beautiful/init.lua
@@ -227,6 +227,10 @@ function beautiful.mt:__index(k)
     return theme[k]
 end
 
+function beautiful.mt:__newindex(k, v)
+    theme[k] = v
+end
+
 -- Set the default font
 set_font("sans 8")
 

commit 69a0de3f03df473d81781e2c89277482c70f0220

Make beautiful.theme_path actually work
Commit ac8af66005 added beautiful.theme_path, which is used to save the
directory that contains the theme file that was loaded. Just two months
later, commit ca12473584 broke this code by adding a __newindex
metamethod. This caused the assignment to beautiful.theme_path to be
redirected to the theme. However, the theme is immediately replaced by
beautiful.init() after setting up the theme_path, so this assignment got
lost.

Fix this by using rawset() to bypass the metamethod.

Fixes: https://github.com/awesomeWM/awesome/issues/2573
Signed-off-by: Uli Schlachter <psychon@znc.in>

lib/beautiful/init.lua
@@ -194,7 +194,7 @@ function beautiful.init(config)
             -- Expand the '~' $HOME shortcut
             config = config:gsub("^~/", homedir .. "/")
             local dir = Gio.File.new_for_path(config):get_parent()
-            beautiful.theme_path = dir and (dir:get_path().."/") or nil
+            rawset(beautiful, "theme_path", dir and (dir:get_path().."/") or nil)
             theme = protected_call(dofile, config)
         elseif type(config) == 'table' then
             theme = config