4b76ca9d385236e0ba5b6f83c3a8fae9c64a701e
Emmanuel Lepage Vallee <elv1313@gmail.com>
2019-01-13 23:56:11 -0500

    doc: Fix awful.hotkeys_popup.show_help formatting.

--- lib/awful/hotkeys_popup/init.lua
+++ lib/awful/hotkeys_popup/init.lua
@@ -10,15 +10,22 @@ local hotkeys_popup = {
   widget = require("awful.hotkeys_popup.widget"),
 }
 
---- This is the same as awful.hotkeys_popup.widget.show_help
+--- This is the same as awful.hotkeys_popup.widget.show_help.
 --
 -- example usage:
+--
 --     local hotkeys_popup = require("awful.hotkeys_popup")
 --     myawesomemenu = {{ "hotkeys", function() hotkeys_popup.show_help() end },
 --                      -- <more entries>
 --     }
 --
 -- see `awful.hotkeys_popup.widget.show_help` for more information
+-- @tparam[opt] client c The hostkeys for the client "c".
+-- @tparam[opt] screen s The screen.
+-- @tparam[opt=true] boolean show_args.show_awesome_keys Show AwesomeWM hotkeys.
+-- When set to `false` only app-specific hotkeys will be shown.
+-- @function awful.hotkeys_popup.show_help
+-- @see awful.hotkeys_popup.widget.show_help
 
 hotkeys_popup.show_help = hotkeys_popup.widget.show_help
 return hotkeys_popup
