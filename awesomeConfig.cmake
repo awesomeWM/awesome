2cb5a0f0e101e6a4a0aa8d6978a95486901bb5f3
Emmanuel Lepage Vallee <elv1313@gmail.com>
2019-01-21 03:04:15 -0500

    codename: Change for "Too long"

--- awesomeConfig.cmake
+++ awesomeConfig.cmake
@@ -4,7 +4,7 @@ set(PROJECT_AWE_NAME awesome)
 # `git describe` later.
 set(VERSION devel)
 
-set(CODENAME "Human after all")
+set(CODENAME "Too long")
 
 option(WITH_DBUS "build with D-BUS" ON)
 option(GENERATE_MANPAGES "generate manpages" ON)
