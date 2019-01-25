071bf84085aaedcc2c2e4b74abcea75b12883ab5
Donlon McGovern <donlon.e.mcgovern@gmail.com>
2019-01-24 22:05:18 -0600

    Fix terminal exit after selecting manual from menu

--- awesomerc.lua
+++ awesomerc.lua
@@ -105,7 +105,7 @@ end
 -- Create a launcher widget and a main menu
 myawesomemenu = {
    { "hotkeys", function() hotkeys_popup.show_help(nil, awful.screen.focused()) end },
-   { "manual", terminal .. " -e man awesome" },
+   { "manual", terminal .. " -e \"man awesome\"" },
    { "edit config", editor_cmd .. " " .. awesome.conffile },
    { "restart", awesome.restart },
    { "quit", function() awesome.quit() end },
