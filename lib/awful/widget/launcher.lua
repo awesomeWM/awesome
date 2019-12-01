cdf322b631491c39ac530781efc961a72c3fa731
Seth Barberee <seth.barberee@gmail.com>
2019-12-01 17:29:16 -0600

    more docs for launcher Co-Authored-By: Aire-One <Aire-One@users.noreply.github.com>

--- lib/awful/widget/launcher.lua
+++ lib/awful/widget/launcher.lua
@@ -1,4 +1,13 @@
 ---------------------------------------------------------------------------
+--  A button widget which hosts a menu or starts a command.
+--
+--  Implementation of `awful.widget.button` with the ability to host an
+--  `awful.menu` or a command to execute on user click.
+--
+-- This example is the default launcher.
+--
+--@DOC_awful_widget_launcher_default_EXAMPLE@
+--
 -- @author Julien Danjou &lt;julien@danjou.info&gt;
 -- @copyright 2008-2009 Julien Danjou
 -- @widgetmod awful.widget.launcher
@@ -12,10 +21,17 @@ local button = require("awful.button")
 local launcher = { mt = {} }
 
 --- Create a button widget which will launch a command.
--- @param args Standard widget table arguments, plus image for the image path
--- and command for the command to run on click, or either menu to create menu.
--- @return A launcher widget.
+---
+-- **NOTE**: You need either command or menu argument for widget to create
+-- successfully
+-- @tparam table args
+-- @tparam image args.image The image to display on the launcher button (refer to `wibox.widget.imagebox`).
+-- @tparam[opt] string args.command Command to run on user click.
+-- @tparam[opt] table args.menu Table of items to create a menu based on `awful.menu`.
+-- @treturn awful.widget.launcher A launcher widget.
 -- @constructorfct awful.widget.launcher
+-- @see wibox.widget.imagebox
+-- @see awful.menu
 function launcher.new(args)
     if not args.command and not args.menu then return end
     local w = wbutton(args)
