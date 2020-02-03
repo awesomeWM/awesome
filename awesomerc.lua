11f0a466b007cf6840b83f3083f3e33ea1cf291a
Emmanuel Lepage Vallee <elv1313@gmail.com>
2020-02-02 20:59:04 -0500

    rc.lua: Add a modeline by default.
    This way people who fork the default config now wont have a bad
    suprise the day 5.0 gets released.

--- awesomerc.lua
+++ awesomerc.lua
@@ -1,3 +1,4 @@
+-- awesome_mode: api-level=4:screen=on
 -- If LuaRocks is installed, make sure that packages installed through it are
 -- found (e.g. lgi). If LuaRocks is not installed, do nothing.
 pcall(require, "luarocks.loader")
