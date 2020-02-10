225bb8a8c63fe3c3248547fdfc18930fc30fc6b2
Uli Schlachter <psychon@znc.in>
2020-02-10 10:15:34 +0100

    Consider icon spacing in systray:draw()
    When less space is available than was asked, systray:draw() has to
    compute the right base size so that all the icons fit into the available
    space. This computation so far ignored the icon spacing, resulting in a
    too large base size.
    
    Fixes: https://github.com/awesomeWM/awesome/issues/2981
    Signed-off-by: Uli Schlachter <psychon@znc.in>

--- spec/wibox/widget/systray_spec.lua
+++ spec/wibox/widget/systray_spec.lua
@@ -50,11 +50,12 @@ describe("wibox.widget.systray", function()
 
     local function test_systray(available_size, expected_size, expected_base)
         systray_arguments = nil
+        local spacing = beautiful_mock.systray_icon_spacing or 0
+
         assert.widget_fit(widget, available_size, expected_size)
         widget:draw(context, cr, unpack(available_size))
-        assert(systray_arguments[4] == expected_base,
-            string.format("Got base size %s, but expected %s", systray_arguments[4], expected_base))
-        assert.is.same(systray_arguments, {true, 0, 0, expected_base, true, '#000000', false, 0})
+        assert.is.near(systray_arguments[4], expected_base, 0.000001)
+        assert.is.same(systray_arguments, {true, 0, 0, systray_arguments[4], true, '#000000', false, spacing})
     end
 
     describe("no spacing", function()
@@ -80,6 +81,37 @@ describe("wibox.widget.systray", function()
             num_systray_icons = 3
             test_systray({ 100, 10 }, { 30, 10 }, 10)
             test_systray({ 100, 100 }, { 100, 100 }, 100 / 3)
+        end)
+    end)
+
+    describe("10 spacing", function()
+        beautiful_mock.systray_icon_spacing = 10
+
+        it("no icons", function()
+            num_systray_icons = 0
+            test_systray({ 100, 10 }, { 0, 0 }, 10)
+            test_systray({ 100, 100 }, { 0, 0 }, 100)
+        end)
+
+        it("one icon", function()
+            num_systray_icons = 1
+            test_systray({ 100, 10 }, { 10, 10 }, 10)
+            test_systray({ 100, 100 }, { 100, 100 }, 100)
+        end)
+
+        it("two icons", function()
+            num_systray_icons = 2
+            test_systray({ 100, 10 }, { 30, 10 }, 10)
+            -- Okay, so we want to place to icons next to each other in a square
+            -- of size 100x100. Between them, there should be 10 pixels of
+            -- space. So, we got 90 pixels for the icons, so 45 px per icon.
+            test_systray({ 100, 100 }, { 100, 100 }, (100 - 10) / 2)
+        end)
+
+        it("three icons", function()
+            num_systray_icons = 3
+            test_systray({ 100, 10 }, { 50, 10 }, 10)
+            test_systray({ 100, 100 }, { 100, 100 }, (100 - 2 * 10) / 3)
         end)
     end)
 end)
