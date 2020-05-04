1518b0f4c0e533b9cf076f0b1f418c99e4d20747
Unai <29188654+unai-ndz@users.noreply.github.com>
2020-05-04 10:42:09 +0200

    Border_marked naming consistency (#3084)
    * finish renaming border_marked to border_color_marked
    
    * doc: document border_color_fullscreen, update border_color_marked and fix typo
    
    * deprecate instead of removing
    
    * doc: fix border_color_marked

--- lib/awful/permissions/init.lua
+++ lib/awful/permissions/init.lua
@@ -543,6 +543,7 @@ end, "mouse_enter")
 -- (urgent, new, active, normal)
 --
 -- @signalhandler awful.ewmh.update_border
+-- @usebeautiful beautiful.border_color_marked
 -- @usebeautiful beautiful.border_color_active
 -- @usebeautiful beautiful.border_color_normal
 -- @usebeautiful beautiful.border_color_new
@@ -637,9 +638,18 @@ function permissions.update_border(c, context)
         -- never have been added to the core. The documentation claims it works,
         -- even if it has been broken for 90% of AwesomeWM releases ever since
         -- it was added.
-        if c.marked and beautiful.border_marked then
-            c._border_color = beautiful.border_marked
-            return
+        if c.marked then
+            if beautiful.border_color_marked then
+                c._border_color = beautiful.border_color_marked
+                return
+            elseif beautiful.border_marked then
+                gdebug.deprecate(
+                    "Use `beautiful.border_color_marked` instead of `beautiful.border_marked`",
+                    {deprecated_in=5}
+                )
+                c._border_color = beautiful.border_marked
+                return
+            end
         end
 
         local tv = beautiful["border_color"..suffix]
