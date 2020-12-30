awesomeWM/awesome

commit c5202a48708585cc33528065af8d1b1d28b1a6e0

tag: Add a context to request::select.

ewmh.c
@@ -462,7 +462,8 @@ ewmh_process_client_message(xcb_client_message_event_t *ev)
         {
             lua_State *L = globalconf_get_lua_State();
             luaA_object_push(L, globalconf.tags.tab[idx]);
-            luaA_object_emit_signal(L, -1, "request::select", 0);
+            lua_pushstring(L, "ewmh");
+            luaA_object_emit_signal(L, -1, "request::select", 1);
             lua_pop(L, 1);
         }
     }

objects/tag.c
@@ -203,8 +203,9 @@
 #include "ewmh.h"
 #include "luaa.h"
 
-/**
+/** When a tag requests to be selected.
  * @signal request::select
+ * @tparam string context The reason why it was called.
  */
 
 /**

commit 339eff9789f4ff50ee27a558181e25ed914291ee

Fix _NET_CURRENT_DESKTOP ClientMessage handling
This was broken in commit c5202a48708585cc33 by adding an extra string
to the Lua stack. Hence, the offset -1 now referred to the string and
not the tag.

Fixes: https://github.com/awesomeWM/awesome/issues/3236
Signed-off-by: Uli Schlachter <psychon@znc.in>

ewmh.c
@@ -468,7 +468,7 @@ ewmh_process_client_message(xcb_client_message_event_t *ev)
             lua_State *L = globalconf_get_lua_State();
             luaA_object_push(L, globalconf.tags.tab[idx]);
             lua_pushstring(L, "ewmh");
-            luaA_object_emit_signal(L, -1, "request::select", 1);
+            luaA_object_emit_signal(L, -2, "request::select", 1);
             lua_pop(L, 1);
         }
     }