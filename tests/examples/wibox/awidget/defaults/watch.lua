Elv13/awesome-1

doc: Complete the widget documentation page.
This adds the `awful.widget` content along with some images.

tests/examples/wibox/awidget/defaults/watch.lua
@@ -1,0 +1,19 @@
+--DOC_HIDE DOC_GEN_IMAGE --DOC_HEADER
+local parent = ... --DOC_NO_USAGE --DOC_HIDE
+local awful = {--DOC_HIDE
+    button = require("awful.button"), --DOC_HIDE
+    widget = {watch = require("awful.widget.watch")} --DOC_HIDE
+}--DOC_HIDE
+local beautiful = require("beautiful") --DOC_HIDE
+local aspawn = require("awful.spawn") --DOC_HIDE
+aspawn.easy_async = function(cmd, cb)--DOC_HIDE
+    cb("Hello world!", "", nil, 0)--DOC_HIDE
+end--DOC_HIDE
+aspawn.with_line_callback = function() end --DOC_HIDE
+
+    local watch = --DOC_HIDE
+    awful.widget.watch('bash -c "echo Hello world! | grep Hello"', 15)
+
+button.forced_height = 24 --DOC_HIDE
+
+parent:add(watch) --DOC_HIDE

awesomeWM/awesome

tests/examples/wibox/awidget/defaults/watch.lua
@@ -1,0 +1,18 @@
+--DOC_HIDE DOC_GEN_IMAGE --DOC_HEADER
+local parent = ... --DOC_NO_USAGE --DOC_HIDE
+local awful = {--DOC_HIDE
+    button = require("awful.button"), --DOC_HIDE
+    widget = {watch = require("awful.widget.watch")} --DOC_HIDE
+}--DOC_HIDE
+local aspawn = require("awful.spawn") --DOC_HIDE
+aspawn.easy_async = function(_, cb)--DOC_HIDE
+    cb("Hello world!", "", nil, 0)--DOC_HIDE
+end--DOC_HIDE
+aspawn.with_line_callback = function() end --DOC_HIDE
+
+    local watch = --DOC_HIDE
+    awful.widget.watch('bash -c "echo Hello world! | grep Hello"', 15)
+
+watch.forced_height = 24 --DOC_HIDE
+
+parent:add(watch) --DOC_HIDE