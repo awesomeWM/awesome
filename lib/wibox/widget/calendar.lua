d625fce450b21ab5190d913ef4b1fdb2cb06968f
mattplm <mpaulmier@gmx.fr>
2021-07-06 17:57:23 +0200

    Stop using fixed row number

--- lib/wibox/widget/calendar.lua
+++ lib/wibox/widget/calendar.lua
@@ -139,8 +139,23 @@ end
 -- @tparam number|nil date.day Date day
 -- @treturn widget Grid layout
 local function create_month(props, date)
-    local num_rows    = 8
-    local num_columns = props.week_numbers and 8 or 7
+    local start_row    = 3
+    local week_start   = props.start_sunday and 1 or 2
+    local last_day     = os.date("*t", os.time{year=date.year, month=date.month+1, day=0})
+    local month_days   = last_day.day
+    local column_fday  = (last_day.wday - month_days + 1 - week_start ) % 7
+
+    local num_columns  = props.week_numbers and 8 or 7
+    local start_column = num_columns - 6
+    local num_rows     = last_day.day // 7
+    if last_day.day > 28 then
+        num_rows = num_rows + 1
+        if column_fday >= 6 then
+            num_rows = num_rows + 1
+        end
+    elseif column_fday > 1 then
+        num_rows = num_rows + 1
+    end
 
     -- Create grid layout
     local layout = grid()
@@ -150,13 +165,6 @@ local function create_month(props, date)
     layout:set_spacing(props.spacing)
     layout:set_forced_num_rows(num_rows)
     layout:set_forced_num_cols(num_columns)
-
-    local start_row    = 3
-    local start_column = num_columns - 6
-    local week_start   = props.start_sunday and 1 or 2
-    local last_day     = os.date("*t", os.time{year=date.year, month=date.month+1, day=0})
-    local month_days   = last_day.day
-    local column_fday  = (last_day.wday - month_days + 1 - week_start ) % 7
 
     --local flags = {"header", "weekdays", "weeknumber", "normal", "focus"}
     local cell_date, t, i, j, w, flag, text
