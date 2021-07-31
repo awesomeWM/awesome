0a68341c128b0c67233f17d2c5c96ed6486f29d3
Lucas Schwiderski <lucas@lschwiderski.de>
2021-07-31 14:55:43 +0200

    doc(w.c.radialprogressbar): Show example code
    Adds the code snippet for the example shown in the module description.
    
    Signed-off-by: Lucas Schwiderski <lucas@lschwiderski.de>

--- tests/examples/wibox/container/defaults/radialprogressbar.lua
+++ tests/examples/wibox/container/defaults/radialprogressbar.lua
@@ -1,6 +1,6 @@
---DOC_HIDE_ALL
+--DOC_HIDE_START
 --DOC_GEN_IMAGE
-local wibox     = require("wibox")
+local wibox = require("wibox")
 
 return {
     text   = "Before",
@@ -9,6 +9,7 @@ return {
     widget = wibox.widget.textbox,
 },
 {
+--DOC_HIDE_END
     {
         {
             text   = "After",
@@ -16,13 +17,14 @@ return {
             valign = "center",
             widget = wibox.widget.textbox,
         },
-        value              = 0.5,
-        max_value          = 1,
-        min_value          = 0,
-        widget             = wibox.container.radialprogressbar
+        value     = 0.5,
+        max_value = 1,
+        min_value = 0,
+        widget    = wibox.container.radialprogressbar
     },
+--DOC_HIDE_START
     margins = 5,
     layout = wibox.container.margin
 }
 
---DOC_HIDE vim: filetype=lua:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:textwidth=80
+-- vim: filetype=lua:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:textwidth=80
