16cdce6d1d048f512c77018eaf941ae7c49f7e29
Lucas Schwiderski <lucas@lschwiderski.de>
2022-01-08 00:24:55 +0100

    refactor(w.l.overflow): Rework unit test file layout
    Improves separation of concerns for individual steps and reworks
    groups and names for clearer output on error.
    
    Signed-off-by: Lucas Schwiderski <lucas@lschwiderski.de>

--- spec/wibox/layout/overflow_spec.lua
+++ spec/wibox/layout/overflow_spec.lua
@@ -3,82 +3,225 @@
 -- @copyright 2021 Lucas Schwiderski
 ---------------------------------------------------------------------------
 
+local spy = require("luassert.spy")
+local match = require("luassert.match")
 local overflow = require("wibox.layout.overflow")
 local base = require("wibox.widget.base")
 local utils = require("wibox.test_utils")
 local p = require("wibox.widget.base").place_widget_at
 
-describe("wibox.layout.overflow", function()
+describe("wibox.layout.overflow.vertical", function()
     local layout
     before_each(function()
         layout = overflow.vertical()
     end)
 
-    it("empty layout fit", function()
-        assert.widget_fit(layout, { 10, 10 }, { 0, 0 })
-    end)
+    describe(":add", function()
+        it("fails without parameters", function()
+            assert.has_error(function()
+                layout:add()
+            end)
+        end)
 
-    it("empty layout layout", function()
-        assert.widget_layout(layout, { 0, 0 }, {})
-    end)
+        it("adds children", function()
+            local w1, w2 = base.empty_widget(), base.empty_widget()
 
-    it("empty add", function()
-        assert.has_error(function()
-            layout:add()
+            assert.is.same({}, layout:get_children())
+
+            layout:add(w1)
+            assert.is.same({ w1 }, layout:get_children())
+
+            layout:add(w2, w2)
+            assert.is.same({ w1, w2, w2 }, layout:get_children())
+        end)
+
+        it("emits signal 'widget::layout_changed'", function()
+            local w1 = base.empty_widget()
+            local s = spy(function() end)
+            layout:connect_signal("widget::layout_changed", function(...)
+                s(...)
+            end)
+
+            layout:add(w1)
+            assert.spy(s).was.called_with(match.is_ref(layout))
         end)
     end)
 
-    describe("with widgets", function()
-        local first, second, third
+    describe(":reset", function()
+        it("removes all widgets", function()
+            local w1, w2 = base.empty_widget(), base.empty_widget()
 
-        before_each(function()
-            first = utils.widget_stub(10, 10)
-            second = utils.widget_stub(15, 15)
-            third = utils.widget_stub(10, 10)
+            layout:add(w1, w2)
+            assert.is.same({ w1, w2 }, layout:get_children())
 
-            layout:add(first, second, third)
+            layout:reset()
+            assert.is.same({}, layout:get_children())
         end)
 
-        describe("with enough space", function()
-            it("fit", function()
+        it("does not fail when already empty", function()
+            layout:reset()
+        end)
+
+        it("emits signal 'widget::layout_changed'", function()
+            local s = spy(function() end)
+            layout:connect_signal("widget::layout_changed", function(...)
+                s(...)
+            end)
+
+            layout:reset()
+            assert.spy(s).was.called_with(match.is_ref(layout))
+        end)
+
+        it("emits signal 'widget::reseted'", function()
+            local s = spy(function() end)
+            layout:connect_signal("widget::reseted", function(...)
+                s(...)
+            end)
+
+            layout:reset()
+            assert.spy(s).was.called_with(match.is_ref(layout))
+        end)
+
+        it("emits signal 'widget::reset'", function()
+            local s = spy(function() end)
+            layout:connect_signal("widget::reset", function(...)
+                s(...)
+            end)
+
+            layout:reset()
+            assert.spy(s).was.called_with(match.is_ref(layout))
+        end)
+    end)
+
+    describe(":fill_space", function()
+        it("emits signal 'widget::layout_changed'", function()
+            local s = spy(function() end)
+            layout:connect_signal("widget::layout_changed", function(...)
+                s(...)
+            end)
+
+            layout:fill_space(false)
+            assert.spy(s).was.called_with(match.is_ref(layout))
+        end)
+
+        it("emits signal 'property::fill_space'", function()
+            local s = spy(function() end)
+            layout:connect_signal("property::fill_space", function(...)
+                s(...)
+            end)
+
+            -- Since the default is `true`, we must use `false` for the desired
+            -- effect
+            layout:fill_space(false)
+            assert.spy(s).was.called_with(match.is_ref(layout), match.is_false())
+
+            -- Assigning the same value again should not do anything
+            layout:fill_space(false)
+            assert.spy(s).was.called(1)
+        end)
+    end)
+
+    describe("spacing", function()
+        it("sets the property", function()
+            layout.spacing = 10
+            assert.is_equal(10, layout.spacing)
+        end)
+
+        it("emits signal 'widget::layout_changed'", function()
+            local s = spy(function() end)
+            layout:connect_signal("widget::layout_changed", function(...)
+                s(...)
+            end)
+
+            layout.spacing = 10
+            assert.spy(s).was.called_with(match.is_ref(layout))
+        end)
+
+        it("emits signal 'property::spacing'", function()
+            local s = spy(function() end)
+            layout:connect_signal("property::spacing", function(...)
+                s(...)
+            end)
+
+            layout.spacing = 10
+            assert.spy(s).was.called_with(match.is_ref(layout), 10)
+
+            -- Assigning the same value again should not do anything
+            layout.spacing = 10
+            assert.spy(s).was.called(1)
+        end)
+    end)
+
+    describe(":fit", function()
+        it("handles empty content", function()
+            assert.widget_fit(layout, { 10, 30 }, { 0, 0 })
+        end)
+
+        describe("with content", function()
+            local first, second, third
+
+            before_each(function()
+                first = utils.widget_stub(10, 10)
+                second = utils.widget_stub(15, 15)
+                third = utils.widget_stub(10, 10)
+
+                layout:add(first, second, third)
+            end)
+
+            it("handles enough space", function()
                 assert.widget_fit(layout, { 100, 100 }, { 15, 35 })
             end)
 
-            it("layout", function()
-                assert.widget_layout(layout, { 100, 100 }, {
-                    p(first,  0,  0, 100, 10),
-                    p(second, 0, 10, 100, 15),
-                    p(third,  0, 25, 100, 10),
-                })
-            end)
-        end)
-
-        describe("without enough width", function()
-            it("fit", function()
+            it("handles missing width", function()
                 assert.widget_fit(layout, { 5, 100 }, { 5, 35 })
             end)
 
-            it("layout", function()
+            it("handles missing height", function()
+                assert.widget_fit(layout, { 30, 20 }, { 20, 20 })
+            end)
+        end)
+    end)
+
+    describe(":layout", function()
+        it("handles empty content", function()
+            assert.widget_layout(layout, { 30, 30 }, {})
+        end)
+
+        describe("with content", function()
+            local first, second, third
+
+            before_each(function()
+                first = utils.widget_stub(10, 10)
+                second = utils.widget_stub(15, 15)
+                third = utils.widget_stub(10, 10)
+
+                layout:add(first, second, third)
+            end)
+
+            it("handles enough space", function()
+                assert.widget_layout(layout, { 30, 100 }, {
+                    p(first,  0,  0, 30, 10),
+                    p(second, 0, 10, 30, 15),
+                    p(third,  0, 25, 30, 10),
+                })
+            end)
+
+            it("handles missing width", function()
                 assert.widget_layout(layout, { 5, 100 }, {
                     p(first,  0,  0, 5, 10),
                     p(second, 0, 10, 5, 15),
                     p(third,  0, 25, 5, 10),
                 })
             end)
-        end)
 
-        describe("without enough height", function()
-            it("fit", function()
-                assert.widget_fit(layout, { 100, 20 }, { 20, 20 })
-            end)
-
-            it("layout", function()
+            it("handles missing height", function()
                 local scrollbar = utils.widget_stub(10, 10)
                 layout:set_scrollbar_widget(scrollbar)
-                assert.widget_layout(layout, { 100, 20 }, {
-                    p(scrollbar,   95,  0,  5, 10),
-                    p(first,  0,  0, 95, 10),
-                    p(second, 0, 10, 95, 15),
+
+                assert.widget_layout(layout, { 30, 20 }, {
+                    p(scrollbar, 25,  0,  5, 10),
+                    p(first,      0,  0, 25, 10),
+                    p(second,     0, 10, 25, 15),
                 })
             end)
         end)
@@ -97,7 +240,7 @@ describe("wibox.layout.overflow", function()
             layout:set_scrollbar_widget(scrollbar)
         end)
 
-        it("to end", function()
+        it("scrolls to end", function()
             assert.widget_layout(layout, { 100, 20 }, {
                 p(scrollbar,   95,  0,  5, 10),
                 p(first,  0,  0, 95, 10),
@@ -114,7 +257,7 @@ describe("wibox.layout.overflow", function()
             })
         end)
 
-        it("one step", function()
+        it("scrolls one step", function()
             assert.widget_layout(layout, { 100, 20 }, {
                 p(scrollbar,   95,  0,  5, 10),
                 p(first,  0,  0, 95, 10),
@@ -129,74 +272,6 @@ describe("wibox.layout.overflow", function()
                 p(second, 0, 5, 95, 15),
             })
         end)
-    end)
-
-    describe("emitting signals", function()
-        local layout_changed
-        before_each(function()
-            layout:connect_signal("widget::layout_changed", function()
-                layout_changed = layout_changed + 1
-            end)
-            layout_changed = 0
-        end)
-
-        it("add", function()
-            local w1, w2 = base.empty_widget(), base.empty_widget()
-            assert.is.equal(0, layout_changed)
-            layout:add(w1)
-            assert.is.equal(1, layout_changed)
-            layout:add(w2)
-            assert.is.equal(2, layout_changed)
-            layout:add(w2)
-            assert.is.equal(3, layout_changed)
-        end)
-
-        it("set_spacing", function()
-            assert.is.equal(0, layout_changed)
-            layout:set_spacing(0)
-            assert.is.equal(0, layout_changed)
-            layout:set_spacing(5)
-            assert.is.equal(1, layout_changed)
-            layout:set_spacing(2)
-            assert.is.equal(2, layout_changed)
-            layout:set_spacing(2)
-            assert.is.equal(2, layout_changed)
-        end)
-
-        it("reset", function()
-            assert.is.equal(0, layout_changed)
-            layout:add(base.make_widget())
-            assert.is.equal(1, layout_changed)
-            layout:reset()
-            assert.is.equal(2, layout_changed)
-        end)
-
-        it("fill_space", function()
-            assert.is.equal(0, layout_changed)
-            layout:fill_space(false)
-            assert.is.equal(1, layout_changed)
-            layout:fill_space(true)
-            assert.is.equal(2, layout_changed)
-            layout:fill_space(true)
-            assert.is.equal(2, layout_changed)
-            layout:fill_space(false)
-            assert.is.equal(3, layout_changed)
-        end)
-    end)
-
-    it("set_children", function()
-        local w1, w2 = base.empty_widget(), base.empty_widget()
-
-        assert.is.same({}, layout:get_children())
-
-        layout:add(w1)
-        assert.is.same({ w1 }, layout:get_children())
-
-        layout:add(w2)
-        assert.is.same({ w1, w2 }, layout:get_children())
-
-        layout:reset()
-        assert.is.same({}, layout:get_children())
     end)
 
     it("can draw `wibox.layout.fixed` as child", function()
