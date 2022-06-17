f14b4f33bdaf499426871fe48c7b4daf136a7a17
rxyhn <rxyhn@proton.me>
2022-06-17 23:02:13 +0700

    feat: night theme

---
+++ themes/night/theme.lua
@@ -0,0 +1,112 @@
+------------------------------------------
+-- Aesthetic Night awesomewm theme      --
+-- Created by https://github.com/rxyhn  --
+------------------------------------------
+
+local theme_assets = require("beautiful.theme_assets")
+local xresources = require("beautiful.xresources")
+local rnotification = require("ruled.notification")
+local dpi = xresources.apply_dpi
+
+local gfs = require("gears.filesystem")
+local themes_path = gfs.get_themes_dir()
+
+local theme = {}
+
+theme.font = "sans 8"
+
+theme.bg_normal = "#061115"
+theme.bg_focus = "#0d181c"
+theme.bg_urgent = "#df5b61"
+theme.bg_minimize = "#000a0e"
+theme.bg_systray = theme.bg_normal
+
+theme.fg_normal = "#d9d7d6"
+theme.fg_focus = "#6791c9"
+theme.fg_urgent = "#d9d7d6"
+theme.fg_minimize = "#d9d7d6"
+
+theme.useless_gap = dpi(2)
+theme.border_width = dpi(2)
+theme.border_color_normal = "#061115"
+theme.border_color_active = "#000a0e"
+theme.border_color_marked = "#000a0e"
+
+-- Generate taglist squares:
+local taglist_square_size = dpi(0)
+theme.taglist_squares_sel = theme_assets.taglist_squares_sel(taglist_square_size, theme.fg_normal)
+theme.taglist_squares_unsel = theme_assets.taglist_squares_unsel(taglist_square_size, theme.fg_normal)
+
+-- Variables set for theming the menu:
+theme.menu_submenu_icon = themes_path .. "night/submenu.png"
+theme.menu_height = dpi(15)
+theme.menu_width = dpi(100)
+
+-- Define the image to load
+theme.titlebar_close_button_normal = themes_path .. "night/titlebar/close_normal.png"
+theme.titlebar_close_button_focus = themes_path .. "night/titlebar/close_focus.png"
+
+theme.titlebar_minimize_button_normal = themes_path .. "night/titlebar/minimize_normal.png"
+theme.titlebar_minimize_button_focus = themes_path .. "night/titlebar/minimize_focus.png"
+
+theme.titlebar_ontop_button_normal_inactive = themes_path .. "night/titlebar/ontop_normal_inactive.png"
+theme.titlebar_ontop_button_focus_inactive = themes_path .. "night/titlebar/ontop_focus_inactive.png"
+theme.titlebar_ontop_button_normal_active = themes_path .. "night/titlebar/ontop_normal_active.png"
+theme.titlebar_ontop_button_focus_active = themes_path .. "night/titlebar/ontop_focus_active.png"
+
+theme.titlebar_sticky_button_normal_inactive = themes_path .. "night/titlebar/sticky_normal_inactive.png"
+theme.titlebar_sticky_button_focus_inactive = themes_path .. "night/titlebar/sticky_focus_inactive.png"
+theme.titlebar_sticky_button_normal_active = themes_path .. "night/titlebar/sticky_normal_active.png"
+theme.titlebar_sticky_button_focus_active = themes_path .. "night/titlebar/sticky_focus_active.png"
+
+theme.titlebar_floating_button_normal_inactive = themes_path .. "night/titlebar/floating_normal_inactive.png"
+theme.titlebar_floating_button_focus_inactive = themes_path .. "night/titlebar/floating_focus_inactive.png"
+theme.titlebar_floating_button_normal_active = themes_path .. "night/titlebar/floating_normal_active.png"
+theme.titlebar_floating_button_focus_active = themes_path .. "night/titlebar/floating_focus_active.png"
+
+theme.titlebar_maximized_button_normal_inactive = themes_path .. "night/titlebar/maximized_normal_inactive.png"
+theme.titlebar_maximized_button_focus_inactive = themes_path .. "night/titlebar/maximized_focus_inactive.png"
+theme.titlebar_maximized_button_normal_active = themes_path .. "night/titlebar/maximized_normal_active.png"
+theme.titlebar_maximized_button_focus_active = themes_path .. "night/titlebar/maximized_focus_active.png"
+
+theme.wallpaper = themes_path .. "night/background.png"
+
+-- Recolor Layout icons:
+theme = theme_assets.recolor_layout(theme, theme.fg_normal)
+
+-- You can use your own layout icons like this:
+theme.layout_fairh = themes_path .. "night/layouts/fairhw.png"
+theme.layout_fairv = themes_path .. "night/layouts/fairvw.png"
+theme.layout_floating = themes_path .. "night/layouts/floatingw.png"
+theme.layout_magnifier = themes_path .. "night/layouts/magnifierw.png"
+theme.layout_max = themes_path .. "night/layouts/maxw.png"
+theme.layout_fullscreen = themes_path .. "night/layouts/fullscreenw.png"
+theme.layout_tilebottom = themes_path .. "night/layouts/tilebottomw.png"
+theme.layout_tileleft = themes_path .. "night/layouts/tileleftw.png"
+theme.layout_tile = themes_path .. "night/layouts/tilew.png"
+theme.layout_tiletop = themes_path .. "night/layouts/tiletopw.png"
+theme.layout_spiral = themes_path .. "night/layouts/spiralw.png"
+theme.layout_dwindle = themes_path .. "night/layouts/dwindlew.png"
+theme.layout_cornernw = themes_path .. "night/layouts/cornernww.png"
+theme.layout_cornerne = themes_path .. "night/layouts/cornernew.png"
+theme.layout_cornersw = themes_path .. "night/layouts/cornersww.png"
+theme.layout_cornerse = themes_path .. "night/layouts/cornersew.png"
+
+-- Generate Awesome icon:
+theme.awesome_icon = theme_assets.awesome_icon(theme.menu_height, "#535d6c", theme.bg_minimize)
+
+-- Define the icon theme for application icons. If not set then the icons
+-- from /usr/share/icons and /usr/share/icons/hicolor will be used.
+theme.icon_theme = nil
+
+-- Set different colors for urgent notifications.
+rnotification.connect_signal("request::rules", function()
+	rnotification.append_rule({
+		rule = { urgency = "critical" },
+		properties = { bg = "#df5b61", fg = "#d9d7d6" },
+	})
+end)
+
+return theme
+
+-- vim: filetype=lua:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:textwidth=80
