40f043d5d03df2ba4ada37b2be72c0e81f601e0a
rxyhn <rxyhn@proton.me>
2022-06-18 19:29:43 +0700

    chore: themes_path and theme variable update

--- themes/night/theme.lua
+++ themes/night/theme.lua
@@ -9,7 +9,7 @@ local rnotification = require("ruled.notification")
 local dpi = xresources.apply_dpi
 
 local gfs = require("gears.filesystem")
-local themes_path = gfs.get_themes_dir()
+local themes_path = gfs.get_themes_dir() .. "night/"
 
 local theme = {}
 
@@ -38,59 +38,63 @@ theme.taglist_squares_sel = theme_assets.taglist_squares_sel(taglist_square_size
 theme.taglist_squares_unsel = theme_assets.taglist_squares_unsel(taglist_square_size, theme.fg_normal)
 
 -- Variables set for theming the menu:
-theme.menu_submenu_icon = themes_path .. "night/submenu.png"
+theme.menu_submenu_icon = themes_path .. "submenu.png"
 theme.menu_height = dpi(15)
 theme.menu_width = dpi(100)
 
+-- Notification
+theme.notification_border_color = theme.border_color_active
+
 -- Define the image to load
-theme.titlebar_close_button_normal = themes_path .. "night/titlebar/close_normal.png"
-theme.titlebar_close_button_focus = themes_path .. "night/titlebar/close_focus.png"
+theme.titlebar_close_button_normal = themes_path .. "titlebar/close_normal.png"
+theme.titlebar_close_button_focus = themes_path .. "titlebar/close_focus.png"
 
-theme.titlebar_minimize_button_normal = themes_path .. "night/titlebar/minimize_normal.png"
-theme.titlebar_minimize_button_focus = themes_path .. "night/titlebar/minimize_focus.png"
+theme.titlebar_minimize_button_normal = themes_path .. "titlebar/minimize_normal.png"
+theme.titlebar_minimize_button_focus = themes_path .. "titlebar/minimize_focus.png"
 
-theme.titlebar_ontop_button_normal_inactive = themes_path .. "night/titlebar/ontop_normal_inactive.png"
-theme.titlebar_ontop_button_focus_inactive = themes_path .. "night/titlebar/ontop_focus_inactive.png"
-theme.titlebar_ontop_button_normal_active = themes_path .. "night/titlebar/ontop_normal_active.png"
-theme.titlebar_ontop_button_focus_active = themes_path .. "night/titlebar/ontop_focus_active.png"
+theme.titlebar_ontop_button_normal_inactive = themes_path .. "titlebar/ontop_normal_inactive.png"
+theme.titlebar_ontop_button_focus_inactive = themes_path .. "titlebar/ontop_focus_inactive.png"
+theme.titlebar_ontop_button_normal_active = themes_path .. "titlebar/ontop_normal_active.png"
+theme.titlebar_ontop_button_focus_active = themes_path .. "titlebar/ontop_focus_active.png"
 
-theme.titlebar_sticky_button_normal_inactive = themes_path .. "night/titlebar/sticky_normal_inactive.png"
-theme.titlebar_sticky_button_focus_inactive = themes_path .. "night/titlebar/sticky_focus_inactive.png"
-theme.titlebar_sticky_button_normal_active = themes_path .. "night/titlebar/sticky_normal_active.png"
-theme.titlebar_sticky_button_focus_active = themes_path .. "night/titlebar/sticky_focus_active.png"
+theme.titlebar_sticky_button_normal_inactive = themes_path .. "titlebar/sticky_normal_inactive.png"
+theme.titlebar_sticky_button_focus_inactive = themes_path .. "titlebar/sticky_focus_inactive.png"
+theme.titlebar_sticky_button_normal_active = themes_path .. "titlebar/sticky_normal_active.png"
+theme.titlebar_sticky_button_focus_active = themes_path .. "titlebar/sticky_focus_active.png"
 
-theme.titlebar_floating_button_normal_inactive = themes_path .. "night/titlebar/floating_normal_inactive.png"
-theme.titlebar_floating_button_focus_inactive = themes_path .. "night/titlebar/floating_focus_inactive.png"
-theme.titlebar_floating_button_normal_active = themes_path .. "night/titlebar/floating_normal_active.png"
-theme.titlebar_floating_button_focus_active = themes_path .. "night/titlebar/floating_focus_active.png"
+theme.titlebar_floating_button_normal_inactive = themes_path .. "titlebar/floating_normal_inactive.png"
+theme.titlebar_floating_button_focus_inactive = themes_path .. "titlebar/floating_focus_inactive.png"
+theme.titlebar_floating_button_normal_active = themes_path .. "titlebar/floating_normal_active.png"
+theme.titlebar_floating_button_focus_active = themes_path .. "titlebar/floating_focus_active.png"
 
-theme.titlebar_maximized_button_normal_inactive = themes_path .. "night/titlebar/maximized_normal_inactive.png"
-theme.titlebar_maximized_button_focus_inactive = themes_path .. "night/titlebar/maximized_focus_inactive.png"
-theme.titlebar_maximized_button_normal_active = themes_path .. "night/titlebar/maximized_normal_active.png"
-theme.titlebar_maximized_button_focus_active = themes_path .. "night/titlebar/maximized_focus_active.png"
+theme.titlebar_maximized_button_normal_inactive = themes_path .. "titlebar/maximized_normal_inactive.png"
+theme.titlebar_maximized_button_focus_inactive = themes_path .. "titlebar/maximized_focus_inactive.png"
+theme.titlebar_maximized_button_normal_active = themes_path .. "titlebar/maximized_normal_active.png"
+theme.titlebar_maximized_button_focus_active = themes_path .. "titlebar/maximized_focus_active.png"
 
-theme.wallpaper = themes_path .. "night/background.png"
+-- Wallpaper
+theme.wallpaper = themes_path .. "background.png"
 
 -- Recolor Layout icons:
 theme = theme_assets.recolor_layout(theme, theme.fg_normal)
 
 -- You can use your own layout icons like this:
-theme.layout_fairh = themes_path .. "night/layouts/fairhw.png"
-theme.layout_fairv = themes_path .. "night/layouts/fairvw.png"
-theme.layout_floating = themes_path .. "night/layouts/floatingw.png"
-theme.layout_magnifier = themes_path .. "night/layouts/magnifierw.png"
-theme.layout_max = themes_path .. "night/layouts/maxw.png"
-theme.layout_fullscreen = themes_path .. "night/layouts/fullscreenw.png"
-theme.layout_tilebottom = themes_path .. "night/layouts/tilebottomw.png"
-theme.layout_tileleft = themes_path .. "night/layouts/tileleftw.png"
-theme.layout_tile = themes_path .. "night/layouts/tilew.png"
-theme.layout_tiletop = themes_path .. "night/layouts/tiletopw.png"
-theme.layout_spiral = themes_path .. "night/layouts/spiralw.png"
-theme.layout_dwindle = themes_path .. "night/layouts/dwindlew.png"
-theme.layout_cornernw = themes_path .. "night/layouts/cornernww.png"
-theme.layout_cornerne = themes_path .. "night/layouts/cornernew.png"
-theme.layout_cornersw = themes_path .. "night/layouts/cornersww.png"
-theme.layout_cornerse = themes_path .. "night/layouts/cornersew.png"
+theme.layout_fairh = themes_path .. "layouts/fairhw.png"
+theme.layout_fairv = themes_path .. "layouts/fairvw.png"
+theme.layout_floating = themes_path .. "layouts/floatingw.png"
+theme.layout_magnifier = themes_path .. "layouts/magnifierw.png"
+theme.layout_max = themes_path .. "layouts/maxw.png"
+theme.layout_fullscreen = themes_path .. "layouts/fullscreenw.png"
+theme.layout_tilebottom = themes_path .. "layouts/tilebottomw.png"
+theme.layout_tileleft = themes_path .. "layouts/tileleftw.png"
+theme.layout_tile = themes_path .. "layouts/tilew.png"
+theme.layout_tiletop = themes_path .. "layouts/tiletopw.png"
+theme.layout_spiral = themes_path .. "layouts/spiralw.png"
+theme.layout_dwindle = themes_path .. "layouts/dwindlew.png"
+theme.layout_cornernw = themes_path .. "layouts/cornernww.png"
+theme.layout_cornerne = themes_path .. "layouts/cornernew.png"
+theme.layout_cornersw = themes_path .. "layouts/cornersww.png"
+theme.layout_cornerse = themes_path .. "layouts/cornersew.png"
 
 -- Generate Awesome icon:
 theme.awesome_icon = theme_assets.awesome_icon(theme.menu_height, "#535d6c", theme.bg_minimize)
@@ -106,6 +110,13 @@ rnotification.connect_signal("request::rules", function()
 		properties = { bg = "#df5b61", fg = "#d9d7d6" },
 	})
 end)
+
+-- Hotkeys Pop Up
+theme.hotkeys_bg = theme.bg_normal
+theme.hotkeys_fg = theme.fg_normal
+theme.hotkeys_modifiers_fg = theme.fg_normal
+theme.hotkeys_border_color = theme.border_color_active
+theme.hotkeys_group_margin = dpi(50)
 
 return theme
 
