5c79d6362e93c9a7c44a55d7e2653d4a74b393df
rxyhn <rxyhn@proton.me>
2022-06-26 02:06:25 +0700

    chore: inherit default theme

--- themes/night/theme.lua
+++ themes/night/theme.lua
@@ -9,9 +9,11 @@ local rnotification = require("ruled.notification")
 local dpi = xresources.apply_dpi
 
 local gfs = require("gears.filesystem")
-local themes_path = gfs.get_themes_dir() .. "night/"
+local themes_path = gfs.get_themes_dir()
 
-local theme = {}
+-- inherit default theme
+local theme = dofile(themes_path .. "default/theme.lua")
+-- load vector assets' generators for this theme
 
 theme.font = "sans 8"
 
@@ -33,75 +35,34 @@ theme.border_color_active = "#000a0e"
 theme.border_color_marked = "#000a0e"
 
 -- Generate taglist squares:
-local taglist_square_size = dpi(0)
-theme.taglist_squares_sel = theme_assets.taglist_squares_sel(taglist_square_size, theme.fg_normal)
-theme.taglist_squares_unsel = theme_assets.taglist_squares_unsel(taglist_square_size, theme.fg_normal)
+--local taglist_square_size = dpi(4)
+--theme.taglist_squares_sel = theme_assets.taglist_squares_sel(taglist_square_size, theme.fg_normal)
+--theme.taglist_squares_unsel = theme_assets.taglist_squares_unsel(taglist_square_size, theme.fg_normal)
+-- Or disable them:
+theme.taglist_squares_sel = nil
+theme.taglist_squares_unsel = nil
+
+-- Taglist
+theme.taglist_fg_focus = theme.bg_normal
+theme.taglist_bg_focus = theme.fg_focus
 
 -- Variables set for theming the menu:
-theme.menu_submenu_icon = themes_path .. "submenu.png"
+theme.menu_submenu_icon = nil
+theme.menu_submenu = "▸ "
 theme.menu_height = dpi(15)
 theme.menu_width = dpi(100)
 
 -- Notification
 theme.notification_border_color = theme.border_color_active
 
--- Define the image to load
-theme.titlebar_close_button_normal = themes_path .. "titlebar/close_normal.png"
-theme.titlebar_close_button_focus = themes_path .. "titlebar/close_focus.png"
-
-theme.titlebar_minimize_button_normal = themes_path .. "titlebar/minimize_normal.png"
-theme.titlebar_minimize_button_focus = themes_path .. "titlebar/minimize_focus.png"
-
-theme.titlebar_ontop_button_normal_inactive = themes_path .. "titlebar/ontop_normal_inactive.png"
-theme.titlebar_ontop_button_focus_inactive = themes_path .. "titlebar/ontop_focus_inactive.png"
-theme.titlebar_ontop_button_normal_active = themes_path .. "titlebar/ontop_normal_active.png"
-theme.titlebar_ontop_button_focus_active = themes_path .. "titlebar/ontop_focus_active.png"
-
-theme.titlebar_sticky_button_normal_inactive = themes_path .. "titlebar/sticky_normal_inactive.png"
-theme.titlebar_sticky_button_focus_inactive = themes_path .. "titlebar/sticky_focus_inactive.png"
-theme.titlebar_sticky_button_normal_active = themes_path .. "titlebar/sticky_normal_active.png"
-theme.titlebar_sticky_button_focus_active = themes_path .. "titlebar/sticky_focus_active.png"
-
-theme.titlebar_floating_button_normal_inactive = themes_path .. "titlebar/floating_normal_inactive.png"
-theme.titlebar_floating_button_focus_inactive = themes_path .. "titlebar/floating_focus_inactive.png"
-theme.titlebar_floating_button_normal_active = themes_path .. "titlebar/floating_normal_active.png"
-theme.titlebar_floating_button_focus_active = themes_path .. "titlebar/floating_focus_active.png"
-
-theme.titlebar_maximized_button_normal_inactive = themes_path .. "titlebar/maximized_normal_inactive.png"
-theme.titlebar_maximized_button_focus_inactive = themes_path .. "titlebar/maximized_focus_inactive.png"
-theme.titlebar_maximized_button_normal_active = themes_path .. "titlebar/maximized_normal_active.png"
-theme.titlebar_maximized_button_focus_active = themes_path .. "titlebar/maximized_focus_active.png"
-
 -- Wallpaper
-theme.wallpaper = themes_path .. "background.png"
+theme.wallpaper = themes_path .. "night/background.png"
 
 -- Recolor Layout icons:
 theme = theme_assets.recolor_layout(theme, theme.fg_normal)
 
--- You can use your own layout icons like this:
-theme.layout_fairh = themes_path .. "layouts/fairhw.png"
-theme.layout_fairv = themes_path .. "layouts/fairvw.png"
-theme.layout_floating = themes_path .. "layouts/floatingw.png"
-theme.layout_magnifier = themes_path .. "layouts/magnifierw.png"
-theme.layout_max = themes_path .. "layouts/maxw.png"
-theme.layout_fullscreen = themes_path .. "layouts/fullscreenw.png"
-theme.layout_tilebottom = themes_path .. "layouts/tilebottomw.png"
-theme.layout_tileleft = themes_path .. "layouts/tileleftw.png"
-theme.layout_tile = themes_path .. "layouts/tilew.png"
-theme.layout_tiletop = themes_path .. "layouts/tiletopw.png"
-theme.layout_spiral = themes_path .. "layouts/spiralw.png"
-theme.layout_dwindle = themes_path .. "layouts/dwindlew.png"
-theme.layout_cornernw = themes_path .. "layouts/cornernww.png"
-theme.layout_cornerne = themes_path .. "layouts/cornernew.png"
-theme.layout_cornersw = themes_path .. "layouts/cornersww.png"
-theme.layout_cornerse = themes_path .. "layouts/cornersew.png"
-
 -- Generate Awesome icon:
-theme.awesome_icon = theme_assets.awesome_icon(theme.menu_height, "#535d6c", theme.bg_minimize)
-
--- Define the icon theme for application icons. If not set then the icons
--- from /usr/share/icons and /usr/share/icons/hicolor will be used.
-theme.icon_theme = nil
+theme.awesome_icon = theme_assets.awesome_icon(theme.menu_height, theme.fg_focus, theme.bg_minimize)
 
 -- Set different colors for urgent notifications.
 rnotification.connect_signal("request::rules", function()
