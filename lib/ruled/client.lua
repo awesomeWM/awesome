awesomeWM/awesome

commit 75019c3f6f6693eecdb08e010957f9e1c3f136bc

Implement fixes for Luacheck 0.26.1
 * Rename or mute parameters starting with _
 * Replace `not (>=)` with `<`

lib/ruled/client.lua
@@ -152,22 +152,22 @@ end
 
 --- Get list of matching rules for a client.
 -- @tparam client c The client.
--- @tparam table _rules The rules to check. List with "rule", "rule_any", "except" and
+-- @tparam table rules The rules to check. List with "rule", "rule_any", "except" and
 --   "except_any" keys.
 -- @treturn table The list of matched rules.
 -- @staticfct ruled.client.matching_rules
-function module.matching_rules(c, _rules)
-    return crules:matching_rules(c, _rules)
+function module.matchingrules(c, rules)
+    return crules:matching_rules(c, rules)
 end
 
 --- Check if a client matches a given set of rules.
 -- @tparam client c The client.
--- @tparam table _rules The rules to check. List of tables with `rule`, `rule_any`,
+-- @tparam table rules The rules to check. List of tables with `rule`, `rule_any`,
 --   `except` and `except_any` keys.
 -- @treturn bool True if at least one rule is matched, false otherwise.
 -- @staticfct ruled.client.matches_list
-function module.matches_list(c, _rules)
-    return crules:matches_rules(c, _rules)
+function module.matches_list(c, rules)
+    return crules:matchesrules(c, rules)
 end
 
 --- Remove a source.

lib/awful/button.lua
@@ -258,14 +258,14 @@ local obj_mt = {
 -- @tparam function args.on_release Callback for when the button is released.
 -- @treturn table An `awful.button` object.
 
-local function new_common(mod, _button, press, release)
+local function new_common(mod, btn, press, release)
     local ret = {}
     local subsets = gmath.subsets(ignore_modifiers)
 
     for _, set in ipairs(subsets) do
         local sub_button = capi.button {
             modifiers = gtable.join(mod, set),
-            button    = _button
+            button    = btn
         }
 
         sub_button._private._legacy_convert_to = ret
@@ -294,9 +294,9 @@ local function new_common(mod, _button, press, release)
     return setmetatable(ret, obj_mt)
 end
 
-function button.new(args, _button, press, release)
+function button.new(args, btn, press, release)
     -- Assume this is the new constructor.
-    if not _button then
+    if not btn then
         assert(not (press or release), "Calling awful.button() requires a button name")
         return new_common(
             args.modifiers,
@@ -305,7 +305,7 @@ function button.new(args, _button, press, release)
             args.on_release
         )
     else
-        return new_common(args, _button, press, release)
+        return new_common(args, btn, press, release)
     end
 end
 

lib/awful/key.lua
@@ -271,7 +271,7 @@ end
 -- @treturn table A table with one or several key objects.
 -- @constructorfct awful.key
 
-local function new_common(mod, _keys, press, release, data)
+local function new_common(mod, keys, press, release, data)
     if type(release)=='table' then
         data=release
         release=nil
@@ -279,7 +279,7 @@ local function new_common(mod, _keys, press, release, data)
 
     local ret = {}
     local subsets = gmath.subsets(key.ignore_modifiers)
-    for _, key_pair in ipairs(_keys) do
+    for _, key_pair in ipairs(keys) do
         for _, set in ipairs(subsets) do
             local sub_key = capi.key {
                 modifiers = gtable.join(mod, set),
@@ -315,15 +315,15 @@ local function new_common(mod, _keys, press, release, data)
     -- append custom userdata (like description) to a hotkey
     data = data and gtable.clone(data) or {}
     data.mod = mod
-    data.keys = _keys
+    data.keys = keys
     data.on_press = press
     data.on_release = release
     data._is_capi_key = false
     assert((not data.key) or type(data.key) == "string")
     table.insert(key.hotkeys, data)
     data.execute = function(_)
-        assert(#_keys == 1, "key:execute() makes no sense for groups")
-        key.execute(mod, _keys[1])
+        assert(#keys == 1, "key:execute() makes no sense for groups")
+        key.execute(mod, keys[1])
     end
 
     -- Store the private data
@@ -412,9 +412,9 @@ local function get_keys(args)
     return key.keygroups[args.keygroup]
 end
 
-function key.new(args, _key, press, release, data)
+function key.new(args, keycode, press, release, data)
     -- Assume this is the new constructor.
-    if not _key then
+    if not keycode then
         assert(not (press or release or data), "Calling awful.key() requires a key name")
 
         local keys = get_keys(args)
@@ -427,7 +427,7 @@ function key.new(args, _key, press, release, data)
             args
         )
     else
-        return new_common(args, {{_key}}, press, release, data)
+        return new_common(args, {{keycode}}, press, release, data)
     end
 end
 
@@ -436,11 +436,11 @@ end
 -- @param pressed_mod The modifiers to compare with.
 -- @param pressed_key The key to compare with.
 -- @staticfct awful.key.match
-function key.match(_key, pressed_mod, pressed_key)
+function key.match(self, pressed_mod, pressed_key)
     -- First, compare key.
-    if pressed_key ~= _key.key then return false end
+    if pressed_key ~= self.key then return false end
     -- Then, compare mod
-    local mod = _key.modifiers
+    local mod = self.modifiers
     -- For each modifier of the key object, check that the modifier has been
     -- pressed.
     for _, m in ipairs(mod) do

lib/awful/keygrabber.lua
@@ -480,7 +480,7 @@ end
 -- @method stop
 -- @emits stopped
 -- @emits property::current_instance
-function keygrabber:stop(_stop_key, _stop_mods) -- (at)function disables ldoc params
+function keygrabber:stop(_stop_key, _stop_mods) --luacheck: ignore 214
     keygrab.stop(self.grabber)
 
     local timer = self._private.timer
@@ -508,7 +508,7 @@ end
 -- @tparam awful.key key The key.
 -- @tparam string description.group The keybinding group
 
-function keygrabber:add_keybinding(key, _keycode, _callback, _description)
+function keygrabber:add_keybinding(key, _keycode, _callback, _description) --luacheck: ignore 214
     local mods = not key._is_awful_key and key or nil
 
     if mods then

lib/awful/layout/init.lua
@@ -145,12 +145,12 @@ function layout.inc(i, s, layouts)
 end
 
 --- Set the layout function of the current tag.
--- @param _layout Layout name.
+-- @tparam layout|function l Layout object or function.
 -- @tparam[opt=mouse.screen.selected_tag] tag t The tag to modify.
 -- @staticfct awful.layout.set
-function layout.set(_layout, t)
+function layout.set(l, t)
     t = t or capi.mouse.screen.selected_tag
-    t.layout = _layout
+    t.layout = l
 end
 
 --- Get the layout parameters used for the screen

lib/awful/layout/suit/floating.lua
@@ -33,71 +33,80 @@ function floating.mouse_resize_handler(c, corner, x, y)
     local fixed_y = c.maximized_vertical
 
     local prev_coords = {}
-    local coordinates_delta = {x=0,y=0}
+    local coordinates_delta = {x=0, y=0}
+
     if floating.resize_jump_to_corner then
       -- Warp mouse pointer
       capi.mouse.coords({ x = x, y = y })
     else
       local corner_x, corner_y = x, y
       local mouse_coords = capi.mouse.coords()
+
       x = mouse_coords.x
       y = mouse_coords.y
-      coordinates_delta = {x=corner_x-x,y=corner_y-y}
+      coordinates_delta = {x = corner_x-x,y = corner_y-y}
     end
 
-    capi.mousegrabber.run(function (_mouse)
-                              if not c.valid then return false end
+    capi.mousegrabber.run(function (state)
+        if not c.valid then return false end
 
-                              _mouse.x = _mouse.x + coordinates_delta.x
-                              _mouse.y = _mouse.y + coordinates_delta.y
-                              for _, v in ipairs(_mouse.buttons) do
-                                  if v then
-                                      local ng
-                                      prev_coords = { x =_mouse.x, y = _mouse.y }
-                                      if corner == "bottom_right" then
-                                          ng = { width = _mouse.x - g.x,
-                                                 height = _mouse.y - g.y }
-                                      elseif corner == "bottom_left" then
-                                          ng = { x = _mouse.x,
-                                                 width = (g.x + g.width) - _mouse.x,
-                                                 height = _mouse.y - g.y }
-                                      elseif corner == "top_left" then
-                                          ng = { x = _mouse.x,
-                                                 width = (g.x + g.width) - _mouse.x,
-                                                 y = _mouse.y,
-                                                 height = (g.y + g.height) - _mouse.y }
-                                      else
-                                          ng = { width = _mouse.x - g.x,
-                                                 y = _mouse.y,
-                                                 height = (g.y + g.height) - _mouse.y }
-                                      end
-                                      if ng.width <= 0 then ng.width = nil end
-                                      if ng.height <= 0 then ng.height = nil end
-                                      if fixed_x then ng.width = g.width ng.x = g.x end
-                                      if fixed_y then ng.height = g.height ng.y = g.y end
-                                      c:geometry(ng)
-                                      -- Get real geometry that has been applied
-                                      -- in case we honor size hints
-                                      -- XXX: This should be rewritten when size
-                                      -- hints are available from Lua.
-                                      local rg = c:geometry()
+        state.x = state.x + coordinates_delta.x
+        state.y = state.y + coordinates_delta.y
 
-                                      if corner == "bottom_right" then
-                                          ng = {}
-                                      elseif corner == "bottom_left" then
-                                          ng = { x = (g.x + g.width) - rg.width  }
-                                      elseif corner == "top_left" then
-                                          ng = { x = (g.x + g.width) - rg.width,
-                                                 y = (g.y + g.height) - rg.height }
-                                      else
-                                          ng = { y = (g.y + g.height) - rg.height }
-                                      end
-                                      c:geometry({ x = ng.x, y = ng.y })
-                                      return true
-                                  end
-                              end
-                              return prev_coords.x == _mouse.x and prev_coords.y == _mouse.y
-                          end, corner .. "_corner")
+        for _, v in ipairs(state.buttons) do
+            if v then
+                local ng
+
+                prev_coords = { x = state.x, y = state.y }
+
+                if corner == "bottom_right" then
+                    ng = { width = state.x - g.x,
+                            height = state.y - g.y }
+                elseif corner == "bottom_left" then
+                    ng = { x = state.x,
+                            width = (g.x + g.width) - state.x,
+                            height = state.y - g.y }
+                elseif corner == "top_left" then
+                    ng = { x = state.x,
+                            width = (g.x + g.width) - state.x,
+                            y = state.y,
+                            height = (g.y + g.height) - state.y }
+                else
+                    ng = { width = state.x - g.x,
+                            y = state.y,
+                            height = (g.y + g.height) - state.y }
+                end
+
+                if ng.width <= 0 then ng.width = nil end
+                if ng.height <= 0 then ng.height = nil end
+                if fixed_x then ng.width = g.width ng.x = g.x end
+                if fixed_y then ng.height = g.height ng.y = g.y end
+
+                c:geometry(ng)
+                -- Get real geometry that has been applied
+                -- in case we honor size hints
+                -- XXX: This should be rewritten when size
+                -- hints are available from Lua.
+                local rg = c:geometry()
+
+                if corner == "bottom_right" then
+                    ng = {}
+                elseif corner == "bottom_left" then
+                    ng = { x = (g.x + g.width) - rg.width  }
+                elseif corner == "top_left" then
+                    ng = { x = (g.x + g.width) - rg.width,
+                            y = (g.y + g.height) - rg.height }
+                else
+                    ng = { y = (g.y + g.height) - rg.height }
+                end
+
+                c:geometry({ x = ng.x, y = ng.y })
+                return true
+            end
+        end
+
+        return prev_coords.x == state.x and prev_coords.y == state.y
+    end, corner .. "_corner")
 end
 
 function floating.arrange()

lib/awful/layout/suit/magnifier.lua
@@ -33,14 +33,14 @@ function magnifier.mouse_resize_handler(c, corner, x, y)
     local maxdist_pow = (wa.width^2 + wa.height^2) / 4
 
     local prev_coords = {}
-    capi.mousegrabber.run(function (_mouse)
+    capi.mousegrabber.run(function (position)
                               if not c.valid then return false end
 
-                              for _, v in ipairs(_mouse.buttons) do
+                              for _, v in ipairs(position.buttons) do
                                   if v then
-                                      prev_coords = { x =_mouse.x, y = _mouse.y }
-                                      local dx = center_x - _mouse.x
-                                      local dy = center_y - _mouse.y
+                                      prev_coords = { x =position.x, y = position.y }
+                                      local dx = center_x - position.x
+                                      local dy = center_y - position.y
                                       local dist = dx^2 + dy^2
 
                                       -- New master width factor
@@ -50,7 +50,7 @@ function magnifier.mouse_resize_handler(c, corner, x, y)
                                       return true
                                   end
                               end
-                              return prev_coords.x == _mouse.x and prev_coords.y == _mouse.y
+                              return prev_coords.x == position.x and prev_coords.y == position.y
                           end, corner .. "_corner")
 end
 

lib/awful/layout/suit/spiral.lua
@@ -24,7 +24,7 @@ local math = math
 
 local spiral = {}
 
-local function do_spiral(p, _spiral)
+local function do_spiral(p, is_spiral)
     local wa = p.workarea
     local cls = p.clients
     local n = #cls
@@ -43,19 +43,19 @@ local function do_spiral(p, _spiral)
             end
         end
 
-        if k % 4 == 0 and _spiral then
+        if k % 4 == 0 and is_spiral then
             wa.x = wa.x - wa.width
         elseif k % 2 == 0 then
             wa.x = wa.x + old_width
-        elseif k % 4 == 3 and k < n and _spiral then
+        elseif k % 4 == 3 and k < n and is_spiral then
             wa.x = wa.x + math.ceil(old_width / 2)
         end
 
-        if k % 4 == 1 and k ~= 1 and _spiral then
+        if k % 4 == 1 and k ~= 1 and is_spiral then
             wa.y = wa.y - wa.height
         elseif k % 2 == 1 and k ~= 1 then
             wa.y = wa.y + old_height
-        elseif k % 4 == 0 and k < n and _spiral then
+        elseif k % 4 == 0 and k < n and is_spiral then
             wa.y = wa.y + math.ceil(old_height / 2)
         end
 

commit ae2f0f99296136d58747430ee5e6d7f1b7660f15

fix(ruled: client: matching_rules): regression after 75019c3f6f

lib/ruled/client.lua
@@ -156,7 +156,7 @@ end
 --   "except_any" keys.
 -- @treturn table The list of matched rules.
 -- @staticfct ruled.client.matching_rules
-function module.matchingrules(c, rules)
+function module.matching_rules(c, rules)
     return crules:matching_rules(c, rules)
 end
 