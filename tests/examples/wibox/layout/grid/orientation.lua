de7f3bfe9033b98234dec44283582cc3db4c77db
Emmanuel Lepage Vallee <elv1313@gmail.com>
2023-12-31 00:42:35 -0800

    grid: Remove an outdated example `print`.

--- tests/examples/wibox/layout/grid/orientation.lua
+++ tests/examples/wibox/layout/grid/orientation.lua
@@ -1,32 +1,23 @@
---DOC_GEN_OUTPUT --DOC_GEN_IMAGE
-local generic_widget = ... --DOC_HIDE_ALL
-local wibox     = require("wibox") --DOC_HIDE
-local beautiful = require("beautiful") --DOC_HIDE
+--DOC_GEN_IMAGE
+local generic_widget = ... --DOC_HIDE_START
+local wibox     = require("wibox")
+local beautiful = require("beautiful")
 
-print([[l = wibox.layout {
-    column_count = 2,
-    row_count    = 2,
-    homogeneous  = true,
-    layout       = wibox.layout.grid
-}
-l:set_orientation("vertical") -- change to "horizontal"
-l:add(...)]]) --DOC_HIDE
-
-return --DOC_HIDE
+return
 wibox.widget {
     {
         {
             markup = "<b>orientation</b> = <i>'vertical'</i>",
             widget = wibox.widget.textbox
         },
-        {
-            {
+        { --DOC_HIDE_STOP
+            {--DOC_HIDE_START
                 generic_widget( "first"  ),
                 generic_widget( "second" ),
                 generic_widget( "third"  ),
                 generic_widget( "fourth" ),
                 generic_widget( "fifth"  ),
-                generic_widget( "sixth"  ),
+                generic_widget( "sixth"  ),--DOC_HIDE_STOP
                 column_count = 2,
                 row_count    = 2,
                 orientation  = "vertical",
@@ -34,7 +25,7 @@ wibox.widget {
                 homogeneous  = true,
                 layout       = wibox.layout.grid,
             },
-            margins = 1,
+            margins = 1,--DOC_HIDE_START
             color   = beautiful.border_color,
             layout  = wibox.container.margin,
         },
@@ -69,4 +60,4 @@ wibox.widget {
     spacing = 5,
     layout = wibox.layout.fixed.horizontal
 }
-, 300, 90 --DOC_HIDE
+, 300, 90
