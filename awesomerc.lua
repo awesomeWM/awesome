754c67f8904a72ec8dd8142d097d2003b7c8c30c
Stardust-kyun <stardust-kyun@proton.me>
2024-05-14 18:00:23 -0400

    revert changes to awesomerc

--- awesomerc.lua
+++ awesomerc.lua
@@ -105,11 +105,16 @@ screen.connect_signal("request::wallpaper", function(s)
     awful.wallpaper {
         screen = s,
         widget = {
-            image  = gears.surface.crop_surface {
-                surface = gears.surface.load_uncached(beautiful.wallpaper),
-                ratio   = s.geometry.width/s.geometry.height,
+            {
+                image     = beautiful.wallpaper,
+                upscale   = true,
+                downscale = true,
+                widget    = wibox.widget.imagebox,
             },
-            widget = wibox.widget.imagebox,
+            valign = "center",
+            halign = "center",
+            tiled  = false,
+            widget = wibox.container.tile,
         }
     }
 end)
