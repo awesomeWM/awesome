03285e8589b5fe2ed6a7dc74fd754db3685116d1
Aidin <149610592+aidinio@users.noreply.github.com>
2024-09-05 17:57:43 +0330

    Comment and documentation improvement (#3943)
    * Added that the timer timeout is in seconds
    
    * Contrasted the differences of autostart and call_now
    
    * A minor grammatical change
    
    * Spread comments in multiple lines so lines would be shorter
    
    * Used three dashes instead of one for documentation
    
    * Included available cursor names as an ldoc files, and also added
    a new file named 'cursor_c_lua.ldoc' which uses awesome comments
    syntax, because the existing 'cursor_c.ldoc' could not be compiled.
    
    * Fixed typo
    
    * Fixed formatting and file names

--- lib/gears/timer.lua
+++ lib/gears/timer.lua
@@ -145,7 +145,7 @@ end
 -- @see start
 -- @see stop
 
---- The timer timeout value.
+--- The timer timeout value in seconds.
 --
 -- @property timeout
 -- @tparam[opt=0] number timeout
@@ -182,8 +182,8 @@ local timer_instance_mt = {
 --
 -- @tparam table args Arguments.
 -- @tparam number args.timeout Timeout in seconds (e.g. `1.5`).
--- @tparam[opt=false] boolean args.autostart Automatically start the timer.
--- @tparam[opt=false] boolean args.call_now Call the callback at timer creation.
+-- @tparam[opt=false] boolean args.autostart Immediately start the timer countdown.
+-- @tparam[opt=false] boolean args.call_now Immediately call the callback function.
 -- @tparam[opt] function args.callback Callback function to connect to the
 --  "timeout" signal.
 -- @tparam[opt=false] boolean args.single_shot Run only once then stop.
