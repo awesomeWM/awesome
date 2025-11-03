a5608b73762f649e63503b585f6c00b9e7c73ca1
Christoph Grüninger <foss@grueninger.de>
2025-11-03 02:41:12 +0100

    Update required CMake support (#4012)
    * Require CMake 3.5 or newer
    
    CMake 4.0 requires that projects require 3.5
    
    * Remove CMake policies to be set to 2.6
    
    Many older policies are no longer supported
    and got removed.
    
    * [cmake] Fix CMP0175 issue
    
    CMake 3.31 requires this keyword when a TARGET is set.
    
    * build: enable symbol exports
    
    Fix CMake CMP0065 policy changes.
    
    ---------
    
    Co-authored-by: Aire-One <aireone@aireone.xyz>

--- awesomeConfig.cmake
+++ awesomeConfig.cmake
@@ -362,6 +362,7 @@ endif()
 
 # {{{ Check for LGI
 add_executable(lgi-check build-utils/lgi-check.c)
+set_target_properties(lgi-check PROPERTIES ENABLE_EXPORTS ON)
 target_link_libraries(lgi-check ${LUA_LIBRARIES})
 target_include_directories(lgi-check PRIVATE ${LUA_INCLUDE_DIR})
 add_custom_target(lgi-check-run ALL
@@ -375,6 +376,7 @@ add_custom_target(lgi-check-run ALL
 add_custom_target(setup_directories DEPENDS lgi-check-run)
 
 add_custom_command(TARGET setup_directories
+        POST_BUILD
         COMMAND ${CMAKE_COMMAND} -E make_directory ${BUILD_DIR}/script_files/
         COMMAND ${CMAKE_COMMAND} -E make_directory ${BUILD_DIR}/docs/common/
         COMMAND ${CMAKE_COMMAND} -E make_directory ${BUILD_DIR}/doc/images/
