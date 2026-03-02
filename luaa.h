dcfd5f69539f57fcef46e2301d9e1afe75290027
cnjhb <hikariz@qq.com>
2026-03-02 17:42:27 +0800

    feat: add luaA_newlib

--- luaa.h
+++ luaa.h
@@ -153,6 +153,15 @@ luaA_setfuncs(lua_State *L, const luaL_Reg *l)
 #endif
 }
 
+#if LUA_VERSION_NUM >= 502
+    #define luaA_newlibtable(L, l) luaL_newlibtable(L, l)
+    #define luaA_newlib(L, l) luaL_newlib(L, l)
+#else
+    #define luaA_newlibtable(L, l) \
+        lua_createtable(L, 0, sizeof(l) / sizeof(l[0]) - 1)
+    #define luaA_newlib(L, l)   (luaA_newlibtable(L, l), luaA_setfuncs(L, l))
+#endif
+
 static inline bool
 luaA_checkboolean(lua_State *L, int n)
 {
